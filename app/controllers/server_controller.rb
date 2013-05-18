0fe7a2af8ad759a387e6bda3ccc6046f11681c8f
Vitaly Shlyaga <storkvist@storkvist.net>
2013-05-18 19:02:27 +0400

    Мониторинг использования диска

--- app/controllers/server_controller.rb
+++ app/controllers/server_controller.rb
@@ -1,5 +1,10 @@
 class ServerController < ApplicationController
   def stats
-    render json: Vmstat.load_average.to_h.merge(Vmstat.memory.to_h)
+    stats = Hash.new
+    stats.merge! Vmstat.load_average.to_h
+    stats.merge! Vmstat.memory.to_h
+    stats.merge! Vmstat.disk('/').to_h
+
+    render json: stats
   end
 end
