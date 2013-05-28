746d9c1399e21a2c86266dbb23e0bb6e8cce61bc
Vitaly Shlyaga <storkvist@storkvist.net>
2013-05-29 00:10:35 +0400

    Редактирование подразделений

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,8 +1,29 @@
 class DepartmentsController < ApplicationController
-  def index
+  before_filter do
+    params[:department] &&= department_params
   end
 
-  def edit
+  before_action :set_department, only: [:edit, :update]
+
+  def index ; end
+
+  def edit ; end
+
+  def update
+    if @department.update(department_params)
+      redirect_to departments_path, notice: 'Изменения сохранены.'
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+  def set_department
     @department = Department.find params[:id]
+  end
+
+  def department_params
+    params.require(:department).permit(:name, :abbreviation, :parent)
   end
 end
