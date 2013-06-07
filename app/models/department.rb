8d5e77a89ff9c6e4dec0e15d824ac0eeae6c3c78
Vitaly Shlyaga <storkvist@storkvist.net>
2013-06-07 22:01:37 +0400

    Вёрстка базовой формы для фильтрации студентов

--- app/models/department.rb
+++ app/models/department.rb
@@ -5,6 +5,7 @@ class Department < ActiveRecord::Base
   alias_attribute :name,         :department_name
   alias_attribute :abbreviation, :department_sname
   alias_attribute :parent,       :department_parent
+  alias_attribute :role,         :department_role
 
   # Подчинённые подразделения текущего подразделения.
   has_many :subdepartments, class_name: Department,
@@ -15,4 +16,6 @@ class Department < ActiveRecord::Base
              foreign_key: :department_parent
 
   scope :only_main, -> { where(department_parent: nil) }
+
+  scope :faculties, -> { where(department_role: :faculty) }
 end
