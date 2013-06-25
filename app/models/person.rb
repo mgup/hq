978360404b6d64c7b178d977cceed59b1b438f9c
Anna Voronkina <vau2007@rambler.ru>
2013-06-25 14:21:39 +0400

    Комментарий

--- app/models/person.rb
+++ app/models/person.rb
@@ -14,31 +14,31 @@ class Person < ActiveRecord::Base
   belongs_to :iname, class_name: Dictionary, primary_key: :dictionary_id, foreign_key: :student_iname
   belongs_to :oname, class_name: Dictionary, primary_key: :dictionary_id, foreign_key: :student_oname
 
-  trigger.after(:insert) do
-    %q(
-      UPDATE student
-      INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
-      INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
-      INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
-      SET student.last_name_hint = fname.dictionary_ip,
-          student.first_name_hint = iname.dictionary_ip,
-          student.patronym_hint = oname.dictionary_ip
-      WHERE student.student_id = NEW.student_id;
-    )
-  end
+  # trigger.after(:insert) do
+  #   %q(
+  #     UPDATE student
+  #     INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
+  #     INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
+  #     INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
+  #     SET student.last_name_hint = fname.dictionary_ip,
+  #         student.first_name_hint = iname.dictionary_ip,
+  #         student.patronym_hint = oname.dictionary_ip
+  #     WHERE student.student_id = NEW.student_id;
+  #   )
+  # end
 
-  trigger.after(:update) do |t|
-    t.where('OLD.student_fname != NEW.student_fname OR OLD.student_iname != NEW.student_iname OR OLD.student_oname != NEW.student_oname') do
-      %q(
-        UPDATE student
-        INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
-        INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
-        INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
-        SET student.last_name_hint = fname.dictionary_ip,
-            student.first_name_hint = iname.dictionary_ip,
-            student.patronym_hint = oname.dictionary_ip
-        WHERE student.student_id = NEW.student_id;
-      )
-    end
-  end
+  # trigger.after(:update) do |t|
+  #   t.where('OLD.student_fname != NEW.student_fname OR OLD.student_iname != NEW.student_iname OR OLD.student_oname != NEW.student_oname') do
+  #     %q(
+  #       UPDATE student
+  #       INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
+  #       INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
+  #       INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
+  #       SET student.last_name_hint = fname.dictionary_ip,
+  #           student.first_name_hint = iname.dictionary_ip,
+  #           student.patronym_hint = oname.dictionary_ip
+  #       WHERE student.student_id = NEW.student_id;
+  #     )
+  #   end
+  # end
 end
--- db/migrate/20130613085347_create_triggers_student_insert_and_student_update.rb
+++ db/migrate/20130613085347_create_triggers_student_insert_and_student_update.rb
@@ -4,46 +4,46 @@
 
 class CreateTriggersStudentInsertAndStudentUpdate < ActiveRecord::Migration
   def up
-    create_trigger("student_after_insert_row_tr", :generated => true, :compatibility => 1).
-        on("student").
-        after(:insert) do
-      <<-SQL_ACTIONS
+    # create_trigger("student_after_insert_row_tr", :generated => true, :compatibility => 1).
+    #     on("student").
+    #     after(:insert) do
+    #   <<-SQL_ACTIONS
 
-      UPDATE student
-      INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
-      INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
-      INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
-      SET student.last_name_hint = fname.dictionary_ip,
-          student.first_name_hint = iname.dictionary_ip,
-          student.patronym_hint = oname.dictionary_ip
-      WHERE student.student_id = NEW.student_id;
-      SQL_ACTIONS
-    end
+    #   UPDATE student
+    #   INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
+    #   INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
+    #   INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
+    #   SET student.last_name_hint = fname.dictionary_ip,
+    #       student.first_name_hint = iname.dictionary_ip,
+    #       student.patronym_hint = oname.dictionary_ip
+    #   WHERE student.student_id = NEW.student_id;
+    #   SQL_ACTIONS
+    # end
 
-    create_trigger("student_after_update_row_tr", :generated => true, :compatibility => 1).
-        on("student").
-        after(:update) do |t|
-      t.where("OLD.student_fname != NEW.student_fname OR OLD.student_iname != NEW.student_iname OR OLD.student_oname != NEW.student_oname") do
-        <<-SQL_ACTIONS
+    # create_trigger("student_after_update_row_tr", :generated => true, :compatibility => 1).
+    #     on("student").
+    #     after(:update) do |t|
+    #   t.where("OLD.student_fname != NEW.student_fname OR OLD.student_iname != NEW.student_iname OR OLD.student_oname != NEW.student_oname") do
+    #     <<-SQL_ACTIONS
 
-        UPDATE student
-        INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
-        INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
-        INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
-        SET student.last_name_hint = fname.dictionary_ip,
-            student.first_name_hint = iname.dictionary_ip,
-            student.patronym_hint = oname.dictionary_ip
-        WHERE student.student_id = NEW.student_id;
-        SQL_ACTIONS
-      end
-    end
+    #     UPDATE student
+    #     INNER JOIN dictionary fname ON student_fname = fname.dictionary_id
+    #     INNER JOIN dictionary iname ON student_iname = iname.dictionary_id
+    #     INNER JOIN dictionary oname ON student_oname = oname.dictionary_id
+    #     SET student.last_name_hint = fname.dictionary_ip,
+    #         student.first_name_hint = iname.dictionary_ip,
+    #         student.patronym_hint = oname.dictionary_ip
+    #     WHERE student.student_id = NEW.student_id;
+    #     SQL_ACTIONS
+    #   end
+    # end
   end
 
   def down
-    drop_trigger("student_after_insert_row_tr", "student", :generated => true)
+    # drop_trigger("student_after_insert_row_tr", "student", :generated => true)
 
-    drop_trigger("student_after_update_row_tr", "student", :generated => true)
+    # drop_trigger("student_after_update_row_tr", "student", :generated => true)
 
-    drop_trigger("student_after_update_row_when_old_student_fname_new_student__tr", "student", :generated => true)
+    # drop_trigger("student_after_update_row_when_old_student_fname_new_student__tr", "student", :generated => true)
   end
 end
