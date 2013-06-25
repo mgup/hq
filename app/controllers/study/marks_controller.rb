1fb77e84f3a022faa1b1de44100d33c959fd61ab
Anna Voronkina <vau2007@rambler.ru>
2013-06-25 17:52:53 +0400

    Ввод данных об оценках

--- app/controllers/study/marks_controller.rb
+++ app/controllers/study/marks_controller.rb
@@ -19,7 +19,35 @@ class Study::MarksController < ApplicationController
   end
 
   def create
-    raise params.inspect
+
+    marks = params[:marks]
+    user= current_user.id
+    # marks.each do |m|
+    #   if m[:mark] != ''
+    #     mark=Study::Mark.new user_id: user, subject_id: params[:subject_id], student_id: m[:student_id], mark: m[:mark], retake: m[:retake]
+    #     mark.save
+    #   end
+    # end
+
+    # redirect_to study_subject_marks_path(@subject)
+
+    ver = true
+    marks.each do |ex|
+      ver= (ex[:mark] != '') && ver
+    end
+
+    if ver
+      marks.each do |m|
+        mark=Study::Mark.new user_id: user, subject_id: params[:subject_id], student_id: m[:student_id], mark: m[:mark], retake: m[:retake]
+        mark.save
+      end
+      redirect_to study_subject_marks_path(@subject), notice: 'Сохранено'
+    else
+      redirect_to :back, notice: 'Вы внесли не все результаты!'
+    end
+
+    
+
     #@mark.session_id = params[:subject_id]
     #if @mark.save
     #  redirect_to new_subject_mark_path(@subject), notice: 'Сохранено'
@@ -38,5 +66,9 @@ class Study::MarksController < ApplicationController
 
   def find_subject
     @subject = Study::Subject.find(params[:subject_id])
+  end
+
+  def find_user
+    @user = User.find(params[:current_user.id])
   end
 end
