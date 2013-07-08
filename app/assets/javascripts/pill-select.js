45259a13b78dcf6fa91c1548a2f86d546600645e
Anna Voronkina <vau2007@rambler.ru>
2013-07-08 22:37:34 +0400

    Текстовое поле для семестра (без обновления курса)

--- app/assets/javascripts/pill-select.js
+++ app/assets/javascripts/pill-select.js
@@ -3,4 +3,18 @@ $(function() {
         var $this = $(this);
         $('#' + $this.data('input')).val($this.data('value'));
     });
+    $('#semester_count').on('change', function() {
+        var $this = $(this);
+        var $semester = $this.val()%2 ? 1 : 2;
+        $('#study_subject_semester').val($semester);
+        var $list = $('#pillsemester li');
+        $list.map(function(){
+        	if ($(this).find('a').data('value') == $semester){
+        		$(this).addClass('active'); 
+        	}
+        	else{
+        		$(this).removeClass('active');
+        	}
+        });
+    });
 });
