83d56678e50c13042eb559b8a5fa0e1c150a45a7
Anna Voronkina <vau2007@rambler.ru>
2013-07-09 12:32:57 +0400

    Текстовое поле для семестра

--- app/assets/javascripts/pill-select.js
+++ app/assets/javascripts/pill-select.js
@@ -5,16 +5,34 @@ $(function() {
     });
     $('#semester_count').on('change', function() {
         var $this = $(this);
-        var $semester = $this.val()%2 ? 1 : 2;
-        $('#study_subject_semester').val($semester);
-        var $list = $('#pillsemester li');
-        $list.map(function(){
-        	if ($(this).find('a').data('value') == $semester){
-        		$(this).addClass('active'); 
-        	}
-        	else{
-        		$(this).removeClass('active');
-        	}
-        });
+        if ((+$this.val() > 12) || (+$this.val() < 1) ||
+            (+$this.val() != parseInt($this.val()))){
+            $this.val('');
+        }
+        else {
+            var $semester = $this.val()%2 ? 1 : 2;
+            var $course = (+$this.val() + ($this.val()%2))/2;
+            $('#study_subject_semester').val($semester);
+            var $list = $('#pillsemester li');
+            $list.map(function(){
+                if ($(this).find('a').data('value') == $semester){
+                    $(this).addClass('active'); 
+                }
+                else{
+                    $(this).removeClass('active');
+                }
+            });
+            $('input#course_study').val($course);
+            var $li = $('#pillcourse li');
+            $li.map(function(){
+                if ($(this).find('a').data('value') == $course){
+                    $(this).addClass('active'); 
+                    $(this).find('a').trigger('shown.bs.tab');
+                }
+                else{
+                    $(this).removeClass('active');
+                }
+            });
+        }
     });
 });
