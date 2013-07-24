6e3196b87875420f9066c0611ac3cc767aca0d6d
Vitaly Shlyaga <storkvist@storkvist.net>
2013-07-24 14:33:30 +0400

    Информация по стоимости обучения

--- app/models/finance/price.rb
+++ app/models/finance/price.rb
@@ -4,7 +4,7 @@ class Finance::Price < ActiveRecord::Base
   alias_attribute :id,          :finance_price_id
   alias_attribute :year,        :finance_price_year
   alias_attribute :semester,    :finance_price_semester
-  alias_attribute :price,       :finance_price_price
+  alias_attribute :sum,         :finance_price_price
 
   belongs_to :payment_type, class_name: Finance::PaymentType, primary_key: :finance_payment_type_id,
              foreign_key: :finance_price_payment_type
