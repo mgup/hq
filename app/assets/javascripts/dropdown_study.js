9adf69d997fed06b2ae0ce0a7dc0a49291fb2693
Anna Voronkina <vau2007@rambler.ru>
2013-07-24 16:19:08 +0400

    исправленная кнопка для Успеваемости

--- app/assets/javascripts/dropdown_study.js
+++ app/assets/javascripts/dropdown_study.js
@@ -5,7 +5,7 @@ $(function(){
     $('li#study').append(
         "<ul class='dropdown-menu' role='menu' aria-labelledby='dLabel'>" +
             "<li><a href='/study/disciplines'>Дисциплины</a></li>" +
-            "<li><a href='/progress/group'>Текущая успеваемость</a></li>" +
+            "<li><a href='/study/group'>Текущая успеваемость</a></li>" +
             "<li><a href='/study/subjects'>Результаты сессий</a></li>" +
         "</ul>"
     )
