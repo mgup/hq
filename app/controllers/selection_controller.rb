daf28e1046d9f44877fcc0548e4501929b7dba90
Anna Voronkina <vau2007@rambler.ru>
2013-07-24 12:33:59 +0400

    Добавления

--- app/controllers/selection_controller.rb
+++ app/controllers/selection_controller.rb
@@ -11,12 +11,17 @@ class SelectionController < ApplicationController
     Student.paid.entrants.each do |student|
       p = Finance::PaymentType.from_student(student).where(finance_payment_type_year: 2013).first
       cost = Finance::Price.where(finance_price_payment_type: p).collect{ |p| p.price * 2}
-      doc = student.documents.where(document_type: 5).first.number  if student.documents.where(document_type: 5).first != nil
+      document = student.documents.where(document_type: 5).first
+      if document != nil
+        doc = document.number
+        date = document.date.strftime("%d.%m.%Y")
+      end
+
       if doc
         payer = student.documents.where(document_type: 5).first.metas.where(document_meta_pattern: 'Плательщик').first.text
       end
 
-      @students << { student: student, cost: cost, document: doc, payer: payer}
+      @students << { student: student, cost: cost, document: doc, payer: payer, date: date}
     end
   end
 
