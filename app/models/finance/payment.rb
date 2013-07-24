dd11bd10a1b25d672ec283446223ae478c829c47
Vitaly Shlyaga <storkvist@storkvist.net>
2013-07-24 15:08:36 +0400

    Ошибка

--- app/models/finance/payment.rb
+++ app/models/finance/payment.rb
@@ -7,6 +7,6 @@ class Finance::Payment < ActiveRecord::Base
 
  belongs_to :payment_type, class_name: Finance::PaymentType, primary_key: :finance_payment_type_id,
            foreign_key: :finance_payment_type
- belongs_to :student, primary_key: :student_id, foreign_key: finance_payment_student_group
+ belongs_to :student, primary_key: :student_id, foreign_key: :finance_payment_student_group
 
 end
