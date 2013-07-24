449070477ac252d18931a710b79882056a5b79be
Anna Voronkina <vau2007@rambler.ru>
2013-07-24 16:08:06 +0400

    Изменила кнопку для Успеваемости

---
+++ app/assets/javascripts/dropdown_study.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('li#study').addClass('dropdown');
+    $('li#study a').addClass('dropdown-toggle').attr('data-toggle','dropdown')
+                   .attr('href','#').append("<span class='caret'></span>");
+    $('li#study').append(
+        "<ul class='dropdown-menu' role='menu' aria-labelledby='dLabel'>" +
+            "<li><a href='/study/disciplines'>Дисциплины</a></li>" +
+            "<li><a href='/progress/group'>Текущая успеваемость</a></li>" +
+            "<li><a href='/study/subjects'>Результаты сессий</a></li>" +
+        "</ul>"
+    )
+    $('.dropdown-toggle').dropdown()
+})
