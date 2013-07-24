5c242b8ccf002c9f48238087c6d80cd6e3fbcff5
Vitaly Shlyaga <storkvist@storkvist.net>
2013-07-24 09:24:58 +0400

    Правки

--- app/models/document/document_student.rb
+++ app/models/document/document_student.rb
@@ -4,6 +4,6 @@ class Document::DocumentStudent < ActiveRecord::Base
   alias_attribute :doc,        :document_student_document
   alias_attribute :student,    :student_id
 
-  belongs_to :doc, class_name: Document::Doc, primary_key: :document_id, foreign_key: :document_student_document
+  belongs_to :document, class_name: Document::Doc, primary_key: :document_id, foreign_key: :document_student_document
   belongs_to :student, primary_key: :student_id, foreign_key: :student_id
 end
