137ceb5fb751f90b2624a8e3b17f3e762f140276
Vitaly Shlyaga <storkvist@storkvist.net>
2013-08-24 15:24:18 +0400

    Вход в личный кабинет студента

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,14 @@
 class DashboardController < ApplicationController
+  skip_before_filter :authenticate_user!
+
   def index
+    # Если авторизовался студент — перенаправляем в личный кабинет студента.
+    if student_signed_in?
+      redirect_to study_groups_path and return
+    end
+
+    redirect_to new_user_session_path and return unless user_signed_in?
+
     redirect_to selection_contract_path if current_user.is?(:zamestitel_otvetstvennogo_sekretarja)
 
     redirect_to selection_contract_path if current_user.is?(:chief_accountant)
