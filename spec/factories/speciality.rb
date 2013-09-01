085b8ab5ba0632e87d4a865c1455be09fc57b880
Anna Voronkina <vau2007@rambler.ru>
2013-09-01 19:32:14 +0400

    Последний раз

--- spec/factories/speciality.rb
+++ spec/factories/speciality.rb
@@ -3,6 +3,7 @@ require 'faker'
 FactoryGirl.define do
   factory :speciality, class: Speciality do
   	name { Faker::Lorem.sentence }
+  	сode { rand(12300) }
   	type { rand(2) }
   	suffix { Faker::Lorem.word }
     speciality_faculty	{ FactoryGirl.create :department }
