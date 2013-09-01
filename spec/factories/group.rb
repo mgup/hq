624431b56fde182b25da3d421d0643ef20bea43f
Anna Voronkina <vau2007@rambler.ru>
2013-09-01 19:41:01 +0400

    Исправлю

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,10 +1,10 @@
-require 'faker'
+# require 'faker'
 
-FactoryGirl.define do
-  factory :group, class: Group do
-  	course { 1 + rand(4) }
-  	number { 1 + rand(2) }
-  	form   { 101 + rand(3) }
-    speciality	{ FactoryGirl.create :speciality }
-  end
-end
+# FactoryGirl.define do
+#   factory :group, class: Group do
+#   	course { 1 + rand(4) }
+#   	number { 1 + rand(2) }
+#   	form   { 101 + rand(3) }
+#     speciality	{ FactoryGirl.create :speciality }
+#   end
+# end
