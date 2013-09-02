994bca10b3509bb164e8c550561a213f81114215
Anna Voronkina <vau2007@rambler.ru>
2013-09-02 12:49:03 +0400

    disciplines_controller_spec.rb

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -1,10 +1,13 @@
-# require 'faker'
+require 'faker'
 
-# FactoryGirl.define do
-#   factory :group, class: Group do
-#   	course { 1 + rand(4) }
-#   	number { 1 + rand(2) }
-#   	form   { 101 + rand(3) }
-#     speciality	{ FactoryGirl.create :speciality }
-#   end
-# end
+FactoryGirl.define do
+   factory :group, class: Group do
+    group_name { Faker::Lorem.word }
+   	course { 1 + rand(4) }
+    group_ncourse { 1 + rand(4) }
+    group_semester {  1 + rand(11) }
+   	number { 1 + rand(2) }
+   	form   { 101 + rand(3) }
+    speciality	{ FactoryGirl.create :speciality }
+   end
+end
