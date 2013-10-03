1a51d67973236b62e775a065aefa25c462496f71
Anna Voronkina <vau2007@rambler.ru>
2013-10-03 11:59:23 +0400

    ЦИОТ "быстрая версия"

--- app/assets/javascripts/ciot.js
+++ app/assets/javascripts/ciot.js
@@ -3,7 +3,28 @@ $(function(){
         div = $(this).closest('div');
         $student =  div.find('input').val();
         $('#edit_student_' + $student).trigger('submit');
-        location.href = '/ciot'
+        $('.modal').modal('hide');
+        $('div#ajax_content table>tbody>tr>td:nth-child(1)').each(function(){
+            if ($(this).text() == $student ){
+                var tr = $(this).closest('tr');
+                $(tr.children().get(5)).html($('#edit_student_' + $student).find('#student_ciot_login').val());
+                $(tr.children().get(6)).html($('#edit_student_' + $student).find('#student_ciot_password').val());
+            }
+        })
+    })
+    function returnValues(elem){
+        div = elem.closest('.modal');
+        $student =  div.find('.modal-footer').find('input').val();
+        var login = $('#edit_student_' + $student).find('#student_ciot_login');
+        var password = $('#edit_student_' + $student).find('#student_ciot_password');
+        login.val(login.attr('value'));
+        password.val(password.attr('value'));
+    }
+    $('.close').click(function(){
+        returnValues($(this));
+    })
+    $('.cancell').click(function(){
+        returnValues($(this));
     })
 
 })
