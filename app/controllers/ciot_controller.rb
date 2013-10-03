0ac3286346f8cddc8110ff206baf63eee2d7ef4c
Anna Voronkina <vau2007@rambler.ru>
2013-10-03 13:45:21 +0400

    Ability ЦИОТ

--- app/controllers/ciot_controller.rb
+++ app/controllers/ciot_controller.rb
@@ -1,6 +1,7 @@
 class CiotController < ApplicationController
 
   def index
+    authorize! :manage, :ciot
     @students = Student.includes([:person, :group]).filter(params).page(params[:page])
   end
 
