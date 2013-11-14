e43a67477352b68e2ffc7502dd5ca6f3a6ee1ad3
Vitaly Shlyaga <storkvist@storkvist.net>
2013-11-14 14:37:40 +0400

    Обновил bundle и чуть-чуть правок в показателях эффективности

--- app/models/achievement_period.rb
+++ app/models/achievement_period.rb
@@ -2,6 +2,14 @@ class AchievementPeriod < ActiveRecord::Base
   has_many :achievements
 
   default_scope do
-    order(:year, :semester)
+    order(:year, :semester).order(active: :desc)
+  end
+
+  def description
+    "#{year}/#{year.to_i + 1} учебный год, #{semester} семестр"
+  end
+
+  def active?
+    active
   end
 end
