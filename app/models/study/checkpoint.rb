83460f78bfe22bf96de21e1126a6db5bc372c3f5
Vitaly Shlyaga <storkvist@storkvist.net>
2013-12-15 20:25:26 +0400

    101,107

--- app/models/study/checkpoint.rb
+++ app/models/study/checkpoint.rb
@@ -37,7 +37,7 @@ class Study::Checkpoint < ActiveRecord::Base
   scope :lectures, -> {where(checkpoint_type: TYPE_LECTURE )}
   scope :practicals, -> {where(checkpoint_type: TYPE_SEMINAR)}
   scope :not_future, -> {where("checkpoint_date < '#{Date.today.strftime('%Y-%m-%d')}'")}
-  scope :not_full, -> discipline {where("checkpoint_subject = #{discipline.id} AND checkpoint_date < '#{Date.today.strftime('%Y-%m-%d')}' AND (SELECT COUNT(DISTINCT checkpoint_mark_student) FROM checkpoint_mark WHERE checkpoint_mark_checkpoint = checkpoint_id) < (SELECT COUNT(*) FROM student_group WHERE student_group_group = #{discipline.group.id} AND student_group_status = '101')")}
+  scope :not_full, -> discipline {where("checkpoint_subject = #{discipline.id} AND checkpoint_date < '#{Date.today.strftime('%Y-%m-%d')}' AND (SELECT COUNT(DISTINCT checkpoint_mark_student) FROM checkpoint_mark WHERE checkpoint_mark_checkpoint = checkpoint_id) < (SELECT COUNT(*) FROM student_group WHERE student_group_group = #{discipline.group.id} AND student_group_status IN ('101', '107'))")}
 
   def lesson
     case type
