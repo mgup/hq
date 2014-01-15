a2314b6e6a6819837564bdd6e7233d0e6b22d5ab
Vitaly Shlyaga <storkvist@storkvist.net>
2014-01-15 12:29:13 +0400

    Работе заведующих кафедр

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -15,10 +15,18 @@ class Achievement < ActiveRecord::Base
   scope :by, -> (user = current_user) { where(user_id: user.id) }
 
   scope :validatable_by, -> (user = current_user) {
-    user_roles = user.roles.map(&:id)
+    user_roles = user.roles.map(&:id).map(&:to_i)
 
     joins(:activity).where('activities.role_id IN (?)', user_roles)
     .order('status ASC, user_id')
+  }
+
+  scope :for_subdepartment, -> (user = current_user) {
+    dep_ids = user.positions.from_role(:subdepartment.to_s).map { |p| p.department.id }
+    users = User.in_department(dep_ids).with_role(Role.select(:acl_role_id).where(acl_role_name: 'lecturer'))
+    ids = users.map { |u| u.id }.push(user.id)
+
+    where(user_id: ids).order('status ASC, user_id')
   }
 
   def new?
