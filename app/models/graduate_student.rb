8dbe4b4b494aa6fb5ccadc721e647e51f79588ed
Vitaly Shlyaga <storkvist@storkvist.net>
2014-02-10 11:08:22 +0400

    Что-то как-то похожее на правду

--- app/models/graduate_student.rb
+++ app/models/graduate_student.rb
@@ -2,5 +2,6 @@ class GraduateStudent < ActiveRecord::Base
   belongs_to :graduate
   belongs_to :student
 
-  has_many :graduate_marks
+  has_many :graduate_marks, -> { includes(:graduate_subject).order('graduate_subjects.kind') }, dependent: :destroy
+  accepts_nested_attributes_for :graduate_marks
 end
--- app/models/graduate_subject.rb
+++ app/models/graduate_subject.rb
@@ -6,4 +6,16 @@ class GraduateSubject < ActiveRecord::Base
 
   has_many :graduate_students
   has_many :graduate_marks
+
+  scope :only_subjects, -> { where(kind: TYPE_SUBJECT) }
+  scope :only_papers, -> { where(kind: TYPE_PAPER) }
+
+  def text_kind
+    case kind
+      when TYPE_SUBJECT
+        'дисициплина'
+      when TYPE_PAPER
+        'курсовая'
+    end
+  end
 end
