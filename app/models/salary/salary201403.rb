f789699766b63c91cf70b38e78b74a3a80f58472
Vitaly Shlyaga <storkvist@storkvist.net>
2014-03-07 13:10:41 +0400

    x

--- app/models/salary/salary201403.rb
+++ app/models/salary/salary201403.rb
@@ -2,4 +2,12 @@ class Salary::Salary201403 < ActiveRecord::Base
   self.table_name = 'salary201403'
 
   belongs_to :user
+
+  #alias_method :old_previous_premium, :previous_premiumgit
+
+  def untouchable?
+    return true if wage_rate.to_f < 1
+
+    untouchable
+  end
 end
