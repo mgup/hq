0b832b36ee287f78f18e795e50be15c076819fcc
Vitaly Shlyaga <storkvist@storkvist.net>
2014-04-17 17:57:13 +0400

    triggers

--- db/migrate/20131008060019_create_trigger_student_insert.rb
+++ db/migrate/20131008060019_create_trigger_student_insert.rb
@@ -4,29 +4,29 @@
 
 class CreateTriggerStudentInsert < ActiveRecord::Migration
   def up
-    create_trigger("student_before_insert_row_tr", :generated => true, :compatibility => 1).
-        on("student").
-        before(:insert) do
-      <<-SQL_ACTIONS
-
-         SET
-            NEW.last_name_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN student ON NEW.student_fname = dictionary.dictionary_id
-                                  LIMIT 1),
-            NEW.first_name_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN student ON NEW.student_iname = dictionary.dictionary_id
-                                  LIMIT 1),
-            NEW.patronym_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN student ON NEW.student_oname = dictionary.dictionary_id
-                                  LIMIT 1);
-      SQL_ACTIONS
-    end
+    # create_trigger("student_before_insert_row_tr", :generated => true, :compatibility => 1).
+    #     on("student").
+    #     before(:insert) do
+    #   <<-SQL_ACTIONS
+    #
+    #      SET
+    #         NEW.last_name_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN student ON NEW.student_fname = dictionary.dictionary_id
+    #                               LIMIT 1),
+    #         NEW.first_name_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN student ON NEW.student_iname = dictionary.dictionary_id
+    #                               LIMIT 1),
+    #         NEW.patronym_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN student ON NEW.student_oname = dictionary.dictionary_id
+    #                               LIMIT 1);
+    #   SQL_ACTIONS
+    # end
   end
 
   def down
-    drop_trigger("student_before_insert_row_tr", "student", :generated => true)
+    # drop_trigger("student_before_insert_row_tr", "student", :generated => true)
   end
 end
--- db/migrate/20140124092558_create_triggers_user_insert_and_user_update.rb
+++ db/migrate/20140124092558_create_triggers_user_insert_and_user_update.rb
@@ -4,56 +4,56 @@
 
 class CreateTriggersUserInsertAndUserUpdate < ActiveRecord::Migration
   def up
-    create_trigger("user_before_insert_row_tr", :generated => true, :compatibility => 1).
-        on("user").
-        before(:insert) do
-      <<-SQL_ACTIONS
-
-         SET
-            NEW.last_name_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN user ON NEW.user_fname = dictionary.dictionary_id
-                                  LIMIT 1),
-            NEW.first_name_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN user ON NEW.user_iname = dictionary.dictionary_id
-                                  LIMIT 1),
-            NEW.patronym_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN user ON NEW.user_oname = dictionary.dictionary_id
-                                  LIMIT 1);
-      SQL_ACTIONS
-    end
-
-    create_trigger("user_before_update_row_tr", :generated => true, :compatibility => 1).
-        on("user").
-        before(:update) do |t|
-      t.where("NEW.user_fname <> OLD.user_fname OR NEW.user_iname <> OLD.user_iname OR\n             NEW.user_oname <> OLD.user_oname") do
-        <<-SQL_ACTIONS
-
-         SET
-            NEW.last_name_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN user ON NEW.user_fname = dictionary.dictionary_id
-                                  LIMIT 1),
-            NEW.first_name_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN user ON NEW.user_iname = dictionary.dictionary_id
-                                  LIMIT 1),
-            NEW.patronym_hint = (SELECT dictionary.dictionary_ip
-                                  FROM dictionary
-                                  JOIN user ON NEW.user_oname = dictionary.dictionary_id
-                                  LIMIT 1);
-        SQL_ACTIONS
-      end
-    end
+    # create_trigger("user_before_insert_row_tr", :generated => true, :compatibility => 1).
+    #     on("user").
+    #     before(:insert) do
+    #   <<-SQL_ACTIONS
+    #
+    #      SET
+    #         NEW.last_name_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN user ON NEW.user_fname = dictionary.dictionary_id
+    #                               LIMIT 1),
+    #         NEW.first_name_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN user ON NEW.user_iname = dictionary.dictionary_id
+    #                               LIMIT 1),
+    #         NEW.patronym_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN user ON NEW.user_oname = dictionary.dictionary_id
+    #                               LIMIT 1);
+    #   SQL_ACTIONS
+    # end
+    #
+    # create_trigger("user_before_update_row_tr", :generated => true, :compatibility => 1).
+    #     on("user").
+    #     before(:update) do |t|
+    #   t.where("NEW.user_fname <> OLD.user_fname OR NEW.user_iname <> OLD.user_iname OR\n             NEW.user_oname <> OLD.user_oname") do
+    #     <<-SQL_ACTIONS
+    #
+    #      SET
+    #         NEW.last_name_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN user ON NEW.user_fname = dictionary.dictionary_id
+    #                               LIMIT 1),
+    #         NEW.first_name_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN user ON NEW.user_iname = dictionary.dictionary_id
+    #                               LIMIT 1),
+    #         NEW.patronym_hint = (SELECT dictionary.dictionary_ip
+    #                               FROM dictionary
+    #                               JOIN user ON NEW.user_oname = dictionary.dictionary_id
+    #                               LIMIT 1);
+    #     SQL_ACTIONS
+    #   end
+    # end
   end
 
   def down
-    drop_trigger("user_before_insert_row_tr", "user", :generated => true)
-
-    drop_trigger("user_before_update_row_tr", "user", :generated => true)
-
-    drop_trigger("user_before_update_row_when_new_user_fname_old_user_fname_or_tr", "user", :generated => true)
+    # drop_trigger("user_before_insert_row_tr", "user", :generated => true)
+    #
+    # drop_trigger("user_before_update_row_tr", "user", :generated => true)
+    #
+    # drop_trigger("user_before_update_row_when_new_user_fname_old_user_fname_or_tr", "user", :generated => true)
   end
 end
