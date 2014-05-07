944587b6400de7ad64b063df98980f0641056da5
Vitaly Shlyaga <storkvist@storkvist.net>
2014-05-07 14:36:35 +0400

    тесты

--- spec/factories/study_discipline.rb
+++ spec/factories/study_discipline.rb
@@ -6,13 +6,14 @@ FactoryGirl.define do
     subject_semester  { 1 + rand(1) }
     subject_name      { Faker::Lorem.sentence }
     subject_brs { true }
-    association :group, factory: :group, strategy: :build
-    association :lead_teacher, factory: :user, strategy: :build
-    association :final_exam,   factory: [:exam, :final], strategy: :build
+    association :group
+    association :lead_teacher, factory: :user
 
-    #after(:build) do |discipline|
-    #  discipline.final_exams << FactoryGirl.build(:final_exam, study_discipline: discipline)
-    #end
+    # association :final_exam,   factory: [:exam, :final]
+
+    # after(:create) do |discipline|
+    #   discipline.final_exam = FactoryGirl.create(:final_exam, exam_subject: discipline)
+    # end
     #
     #factory :discipline_with_checkpoints, parent: :study_discipline do
     #  after(:build) do |discipline|
