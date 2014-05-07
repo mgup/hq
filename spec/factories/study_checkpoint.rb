e71097b02fe4ea5c4939865c0d937c8ddc7906ce
Vitaly Shlyaga <storkvist@storkvist.net>
2014-05-07 15:19:58 +0400

    Результаты переименования trait-ов

--- spec/factories/study_checkpoint.rb
+++ spec/factories/study_checkpoint.rb
@@ -5,16 +5,19 @@ FactoryGirl.define do
     type { 1 + rand(1) }
     date 	{ Date.today }
     discipline
-    trait :checkpoint_control do
+
+    trait :control do
       type { 3 }
       min { 44 }
       max { 80 }
       name { Faker::Lorem.word }
       details { Faker::Lorem.sentence }
     end
+
     trait :lecture do
       type { 1 }
     end
+
     trait :practical do
       type { 2 }
     end
