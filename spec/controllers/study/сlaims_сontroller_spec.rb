d4bf2b5afb5570e9158c0d5e4146904070d9ec92
Kate <pavlintseva.e.d@gmail.com>
2014-05-20 23:06:32 +0400

    claims контроллер

---
+++ spec/controllers/study/сlaims_сontroller_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+     describe 'GET "index"' do
+      before :each do
+        @event = FactoryGirl.create(:event)
+        get :index
+      end
+
+      it 'должен выполняться успешно' do
+        response.should be_success
+      end
+
+      it 'должен выводить правильное представление' do
+        response.should render_template(:index)
+      end
+
+      it 'в выводе должна присутствовать заявка' do
+        assigns(:events).should include(@event)
+      end
+    end
+
