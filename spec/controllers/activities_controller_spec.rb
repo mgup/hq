581fe502ccb822f8375b86af328d2933a598d2cb
Alexander <Ftech42@yandex.ru>
2014-05-21 00:44:37 +0400

    Activities controller spec

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -8,6 +8,7 @@ end
 
 	describe 'GET "index"' do
 		before :each do
+			# @activity = FactoryGirl.create(:activity)
 			get :index
 		end
 		it 'должен выполняться успешно' do
@@ -17,6 +18,45 @@ end
 		it 'должен выводить правильное представление' do
 			response.should render_template(:index)
 		end
-		
-	end 
+
+	end
+	describe 'GET #new' do
+      before :each do
+        get :new
+      end
+
+      it 'должен выполняться успешно' do
+        response.should be_success
+      end
+
+      it 'должен выводить правильное представление' do
+        response.should render_template(:new)
+      end
+
+    end
+    describe 'POST #create' do
+      context 'в случае успешного создания' do
+        before :each do
+          Activity.any_instance.should_receive(:save).and_return(true)
+          post :create, activity: {}
+        end
+
+        it 'должен создавать новый департамент' do
+          flash[:notice].should_not be_nil
+        end
+
+        it 'должно происходить перенаправление на страницу со структурой университета' do
+          response.should redirect_to activities_path
+        end
+      end
+
+      context 'в случае ошибки' do
+        it 'должен перенаправить на создание' do
+          Activity.any_instance.should_receive(:save).and_return(false)
+          post :create, activity: {}
+          response.should render_template :new
+        end
+      end
+    end
+
 end
