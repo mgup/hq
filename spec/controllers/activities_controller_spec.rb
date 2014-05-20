1deb2e6822cd0cb2371d3ef75817a751adce3db3
Alexander <Ftech42@yandex.ru>
2014-05-21 00:23:40 +0400

    Some first test things

---
+++ spec/controllers/activities_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe ActivitiesController do 
+before do
+	@user = create(:user, :developer)
+	sign_in @user
+end
+
+	describe 'GET "index"' do
+		before :each do
+			get :index
+		end
+		it 'должен выполняться успешно' do
+			response.should be_success
+		end
+
+		it 'должен выводить правильное представление' do
+			response.should render_template(:index)
+		end
+		
+	end 
+end
