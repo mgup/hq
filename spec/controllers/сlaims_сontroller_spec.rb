989b39db226ad93a0ce172439347b25a34b7f7e7
Kate <pavlintseva.e.d@gmail.com>
2014-05-21 01:51:23 +0400

    1

--- spec/controllers/сlaims_сontroller_spec.rb
+++ spec/controllers/сlaims_сontroller_spec.rb
@@ -1,21 +1,42 @@
 require 'spec_helper'
 
-     describe 'GET "index"' do
-      before :each do
-        @event = FactoryGirl.create(:event)
-        get :index
-      end
+describe ClaimsController do
+   context 'для разработчиков' do
+    before do
+      @user = create(:user, :developer)
+      sign_in @user
+    end
 
+     describe 'GET "index"' do
       it 'должен выполняться успешно' do
+        get :index
         response.should be_success
       end
 
       it 'должен выводить правильное представление' do
+        get :index
         response.should render_template(:index)
       end
 
-      it 'в выводе должна присутствовать заявка' do
-        assigns(:events).should include(@event)
-      end
+      
     end
+  end
 
+      context 'для пользователей, не являющихся разработчиками,' do
+    it 'должен быть переход на главную страницу' do
+      sign_in FactoryGirl.create(:user)
+
+      get :index
+      response.should redirect_to(root_path)
+    end
+  end
+
+  context 'для не авторизованных пользователей' do
+    it 'должен быть переход на страницу авторизации' do
+      sign_out :user
+
+      get :index
+      response.should redirect_to(new_user_session_path)
+    end
+  end
+  end
