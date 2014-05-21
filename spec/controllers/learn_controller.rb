5334b092bc84aa6513275e4b0cb95e1db725f872
Alinarh <Alinarh93@mail.ru>
2014-05-21 13:01:00 +0300

    Create learn_controller.rb

---
+++ spec/controllers/learn_controller.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+describe LearnController do
+context 'Когда пользователь не найден' do
+  it "Ввести еще раз" 
+end
+context 'Пользователь найден' do
+  it "Продолжить работу"
+end
