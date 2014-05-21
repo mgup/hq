7a79fb77ca9515747afecb2a4fc2fe9b64d42e95
TaniaSPK <spenelopes@rambler.ru>
2014-05-21 13:16:57 +0400

    14

---
+++ spec/controllers/study/subjects_controller_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe Study::SubjectsController do
+  before do
+    @user = create(:user, :developer)
+    sign_in @user
+  end
+
+  describe 'GET "index"' do
+    before :each do
+      get :index
+    end
+    it 'должен выполняться успешно' do
+      response.should be_success
+    end
+
+    it 'должен выводить правильное представление' do
+      response.should render_template(:index)
+    end
+
+  end
+
+  describe 'GET #new' do
+    before :each do
+      get :new
+    end
+
+    it 'должен выполняться успешно' do
+      response.should be_success
+    end
+
+    it 'должен выводить правильное представление' do
+      response.should render_template(:new)
+    end
+
+  end
+  describe 'POST #create' do
+    context 'в случае успешного создания' do
+      before :each do
+        Subject.any_instance.should_receive(:save).and_return(true)
+        post :create, study_subject: {}
+      end
+
+      it 'должен создавать новый Subject' do
+        flash[:notice].should_not be_nil
+      end
+
+      it 'должно происходить перенаправление на страницу со структурой Activity' do
+        response.should redirect_to study_subject_marks_path(@subject)
+      end
+    end
+
+    context 'в случае ошибки' do
+      it 'должен перенаправить на создание' do
+        Subject.any_instance.should_receive(:save).and_return(false)
+        post :create, study_subject: {}
+        response.should render_template :new
+      end
+    end
+  end
+
+end
