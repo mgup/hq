de34e25927d178e0172ce7987a6f879c66e8ef88
Kate <pavlintseva.e.d@gmail.com>
2014-05-21 18:34:48 +0400

    test document model

--- spec/models/curator/task_spec.rb
+++ spec/models/curator/task_spec.rb
@@ -1,21 +1,21 @@
 require 'spec_helper'
-
-describe Curator::Task  do
-it 'должен обладать валидной фабрикой' do
-build(:curator_task).should be_valid
-end
-
-describe 'обладает связями с другими моделями:' do
-it 'с типом задания' do
-should belong_to(:type)
-end
-it 'с индивидуальным заданием' do
-should have_many(:task_users)
-end
-it 'с человеком' do
-should have_many(:users)
-end
-
-end
-
-end
+ 
+ describe Curator::Task  do
+ it 'должен обладать валидной фабрикой' do
+ build(:curator_task).should be_valid
+ end
+ 
+ describe 'обладает связями с другими моделями:' do
+ it 'с типом задания' do
+ should belong_to(:type)
+ end
+ it 'с индивидуальным заданием' do
+ should have_many(:task_users)
+ end
+ it 'с человеком' do
+ should have_many(:users)
+ end
+ 
+ end
+ 
+ end
