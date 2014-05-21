69af58b82b8b7fab152597fdb1aab3312fa32f08
Kate <pavlintseva.e.d@gmail.com>
2014-05-21 14:42:21 +0400

    только index

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -19,42 +19,6 @@ end
     end
 
   end
-  describe 'GET #new' do
-      before :each do
-        get :new
-      end
 
-      it 'должен выполняться успешно' do
-        response.should be_success
-      end
 
-      it 'должен выводить правильное представление' do
-        response.should render_template(:new)
-      end
-
-    end
-    describe 'POST #create' do
-      context 'в случае успешного создания' do
-        before :each do
-          Activity.any_instance.should_receive(:save).and_return(true)
-          post :create, activity: {}
-        end
-
-        it 'должен создавать новый активити' do
-          flash[:notice].should_not be_nil
-        end
-
-        it 'должно происходить перенаправление на страницу со структурой Activity' do
-          response.should redirect_to activities_path
-        end
-      end
-
-      context 'в случае ошибки' do
-        it 'должен перенаправить на создание' do
-          Activity.any_instance.should_receive(:save).and_return(false)
-          post :create, activity: {}
-          response.should render_template :new
-        end
-      end
-    end
 end
