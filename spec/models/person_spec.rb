b9a13072a4feb201a2c9c8382fee38728c8df804
Kate <pavlintseva.e.d@gmail.com>
2014-05-21 21:14:13 +0400

    person model spec

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -5,30 +5,25 @@ describe Person do
 		build(:student).should be_valid
 	end
 
-	describe 'обладает связями с другими моделями' do
+	describe 'обладает связями с другими моделями:' do
 		it 'с фамилией' do
-			should belong_to(:fname)
-		end
+		should belong_to(:fname)
+	end
 
-		it 'с именем' do
-			should belong_to(:iname)
-		end
+	it 'с именем' do
+		should belong_to(:iname)
+	end
 
-		it 'с отчеством' do
-			should have_many(:oname)
-		end
+	it 'с отчеством' do
+		should belong_to(:oname)
+	end
 
-		it 'с с комнатой' do
-			should have_many(:room)
-		end
+	it 'с комнатой' do
+		should belong_to(:room)
+	end
 
-		it 'со студентами' do
-			should have_many(:students)
-		end
-
-		it 'с document_students' do
-			should have_many(:document_students)
-		end
-
-end		
+	it 'с студентами' do
+		should have_many(:students)
+	end
+	end
 end
