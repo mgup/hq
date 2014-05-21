427ff6fd4511845c4b953cf24878bb356ffa977c
Vitaly Shlyaga <storkvist@storkvist.net>
2014-05-22 00:33:55 +0400

    Правки

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -22,54 +22,16 @@ describe Student do
 			should have_many(:exams)
 		end
 
-		it 'с xmarks' do
-			should have_many(:xmarks)
-		end
-
-		it 'с document_students' do
-			should have_many(:document_students)
-		end
-
-		it 'с documents' do
-			should have_many(:documents)
-		end
-
-		# it 'с платежами' do
-		# 	should have_many(:payments)
-		# end
-		# something going wrong here
-
-		it 'с supports' do
+		it 'с заявлениями на материальную помощь' do
 			should have_many(:supports)
 		end
 
-		it 'с selections' do
-			should have_many(:selections)
-		end
-
-		it 'с choices' do
-			should have_many(:choices)
-		end
-
-		it 'с students_in_order' do
+		it 'с вспомогательной таблицей для приказов' do
 			should have_many(:students_in_order)
 		end
 
-		it 'с orders' do
+		it 'с приказами' do
 			should have_many(:orders).through(:students_in_order)
 		end
-
-		it 'с visitor_event_dates' do
-			should have_many(:visitor_event_dates)
-		end
-
-		it 'с dates' do
-			should have_many(:dates).through(:visitor_event_dates)
-		end
-
-		it 'с graduate_student' do
-			should have_one(:graduate_student)
-		end
-		
 	end
 end
