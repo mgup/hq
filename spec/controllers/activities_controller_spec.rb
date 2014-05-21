cfad93fa68d9fce0a9c3aafb3022ebe8424dc84b
Kate <pavlintseva.e.d@gmail.com>
2014-05-21 11:30:53 +0400

    11

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -2,24 +2,24 @@ require 'spec_helper'
 
 describe ActivitiesController do 
 before do
-	@user = create(:user, :developer)
-	sign_in @user
+  @user = create(:user, :developer)
+  sign_in @user
 end
 
-	describe 'GET "index"' do
-		before :each do
-			get :index
-		end
-		it 'должен выполняться успешно' do
-			response.should be_success
-		end
+  describe 'GET "index"' do
+    before :each do
+      get :index
+    end
+    it 'должен выполняться успешно' do
+      response.should be_success
+    end
 
-		it 'должен выводить правильное представление' do
-			response.should render_template(:index)
-		end
+    it 'должен выводить правильное представление' do
+      response.should render_template(:index)
+    end
 
-	end
-	describe 'GET #new' do
+  end
+  describe 'GET #new' do
       before :each do
         get :new
       end
