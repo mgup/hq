587964ffa46b5b4046be7f97eb71652bf24b394c
Vitaly Shlyaga <storkvist@storkvist.net>
2014-05-21 21:10:52 +0400

    Ад перфекциониста

--- spec/models/study/exam_student.rb
+++ spec/models/study/exam_student.rb
@@ -9,13 +9,13 @@ describe Study::ExamStudent do
     it 'с  экзаменом' do
       should belong_to(:exam)
     end
+    
     it 'со студентом' do
       should belong_to(:student)
     end
+    
     it 'с человеком' do
       should belong_to(:person)
     end
-
   end
-
 end
