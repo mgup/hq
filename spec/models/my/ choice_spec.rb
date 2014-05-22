878c0cf319cb2bca9e43678d5529352c3194cc57
Alinarh <Alinarh93@mail.ru>
2014-05-22 19:24:25 +0300

    Create  choice_spec.rb

---
+++ spec/models/my/ choice_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe My::Choice do
+	it 'должен обладать валидной фабрикой' do
+		build(:optional).should be_valid
+	end
+
+	describe 'обладает подразделами' do
+		it 'с пользователем' do
+			should have_many(:selections).class_name('Study::Discipline')
+		end
+
+		it 'со студентами' do
+			should have_many(:students).though(:selections)
+		end
+	end
+end
