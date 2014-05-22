04c152cb8b14131653631ccbe8d7071a3f62ba09
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-22 16:59:07 +0400

    Test

--- spec/models/study/subject_spec.rb
+++ spec/models/study/subject_spec.rb
@@ -1,1 +1,22 @@
-#
+require 'spec_helper'
+
+describe Study::Subject do
+  it 'должен обладать валидной фабрикой' do
+    build(:study_subjects).should be_valid
+  end
+
+  describe 'обладает связями с другими моделями:' do
+    it 'с групой' do
+      should belong_to(:group)
+    end
+    
+    it 'с пользователями' do
+      should belong_to(:user)
+    end
+    
+    it 'с оценками' do
+      should have_many(:marks)
+    end
+  end
+
+end
