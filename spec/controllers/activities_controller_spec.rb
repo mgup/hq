b17055f19547a117701ac1f8730625ed8f583465
Vitaly Shlyaga <storkvist@storkvist.net>
2014-05-22 15:29:50 +0400

    Стиль.

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 describe ActivitiesController do 
-before do
-  @user = create(:user, :developer)
-  sign_in @user
-end
+  before do
+    @user = create(:user, :developer)
+    sign_in @user
+  end
 
   describe 'GET "index"' do
     before :each do
       get :index
     end
+    
     it 'должен выполняться успешно' do
       response.should be_success
     end
@@ -17,8 +18,5 @@ end
     it 'должен выводить правильное представление' do
       response.should render_template(:index)
     end
-
   end
-
-
 end
