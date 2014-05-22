bbfc9c23a060366bf89011c63f2e342da1aff1a7
mary-pin <mary_pin@mail.ru>
2014-05-22 16:03:51 +0300

    Update event_day_spec.rb

--- spec/models/event_day_spec.rb
+++ spec/models/event_day_spec.rb
@@ -2,28 +2,26 @@ require 'spec_helper'
 
 describe EventDate do
 	it 'должен обладать валидной фабрикой' do
-		build(:EventDate).should be_valid
+		build(:event_date).should be_valid
 	end
 
 	describe 'обладает связями с другими моделями' do
-		it 'с пользователем' do
+		it 'с событием' do
 			should belong_to(:event)
 		end
 
-		it 'с ролями' do
-			should belong_to(:class_name)
+		it 'с посетителями события' do
+			should belong_to(:visitor_event_dates)
 		end
-
-		it 'с подразделениями' do
-			should has_many(:visitor_event_dates)
-		end
-
-		it 'с пользователями' do
+	end
+	describe 'иметь много' do
+		it 'пользователей' do
 			should has_many(:users)
 		end
 
-		it 'с назначениями' do
+		it 'студентов' do
 			should has_many(:students)
 		end
 	end
+	
 end
