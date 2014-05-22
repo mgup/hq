af3fcd446a5e2b262ffd05a789df6d5a9de7593e
Vitaly Shlyaga <storkvist@storkvist.net>
2014-05-22 15:31:02 +0400

    Правки.

--- spec/models/study/discipline_teacher_spec.rb
+++ spec/models/study/discipline_teacher_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe Study::DisciplineTeacher do
-  it 'должен обладать валидной фабрикой (через финальный экзамен)' do
+  it 'должен обладать валидной фабрикой' do
     build(:discipline).should be_valid
   end
 
   describe 'обладает связями с другими моделями:' do
-    it 'с ассистентом преподавателя' do
+    it 'с дополнительными преподавателями' do
       should belong_to(:assistant_teacher)
     end
+    
     it 'с дисциплиной' do
       should belong_to(:discipline)
     end
-
   end
 end
