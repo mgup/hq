fdfd652736f9ace580bc9031f452321ee940435d
Alinarh <Alinarh93@mail.ru>
2014-05-22 16:18:04 +0300

    Create  order_reason_spec.rb

---
+++ spec/models/office/ order_reason_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Office::OrderReason do
+	it 'должен обладать валидной фабрикой' do
+		build(:position).should be_valid
+	end
+
+	describe 'обладает связями с другими моделями' do
+		it 'с порядком' do
+			should belong_to(:order)
+		end
+
+		it 'с причинами' do
+			should belong_to(:reason)
+		end
+
+	end
+end
