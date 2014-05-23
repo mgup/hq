f263900cdd1a99ac738279f4c811f433b3a55553
Mariaaaa <mmmasha555@mail.ru>
2014-05-23 21:16:58 +0400

    11

---
+++ spec/models/study/xmark_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Study::Xmark do
+  it 'должен обладать валидной фабрикой' do
+    build(:study_marks).should be_valid
+  end
+
+  describe 'обладает связями с другими моделями:' do
+    it 'с дисциплиной' do
+      should belong_to(:subject)
+    end
+    it 'с пользователь' do
+      should belong_to(:user)
+    end
+     it 'со студентом' do
+      should belong_to(:student)
+    end
+  end
+
+end
