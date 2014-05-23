deb4cd4b03fe8736f74c6ea6062b9f1c144f40fd
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-23 15:30:44 +0400

    Update person_spec.rb

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe Person do
-  it 'должен обладать валидной фабрикой' do
-    build(:student).should be_valid
-  end
-
+  
   describe 'обладает связями с другими моделями:' do
     it 'с фамилией' do
       should  belongs_to(:fname)
