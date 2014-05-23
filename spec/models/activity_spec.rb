858db00653361566bcd5cc3fd72060d74748cd48
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-23 15:26:47 +0400

    Update activity_spec.rb

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -2,28 +2,24 @@ require 'spec_helper'
 describe Activity do
 	
 	describe 'обладает связями с другими моделями:' do
-	    it 'с груповой деятельностью' do
-	      should belongs_to(:activity_group)
-	    end
-
-	    it 'с группой' do
-	      should belongs_to(:group)
-	    end
+		it 'с груповой деятельностью' do
+			should belongs_to(:activity_group)
+			end
+			
+		it 'с группой' do
+			should belongs_to(:group)
+			end
 
 	  	it 'с типом деятельности' do
-	      should belongs_to(:activity_type)
-	    end
+	  		should belongs_to(:activity_type)
+	    		end
 
-	    it 'с типом кредитной деятельности' do
-	      should belongs_to(:activity_credit_type)
-	    end
-
-	    it 'с валидатором' do
-	      should belongs_to(:validator)
-	    end
+	    	it 'с типом кредитной деятельности' do
+	      		should belongs_to(:activity_credit_type)
+	    		end
 	    
   		it 'с достижениями' do
-	      should have_many(:achievements)
-	    end
+	      		should have_many(:achievements)
+	    		end
 	end
 end
