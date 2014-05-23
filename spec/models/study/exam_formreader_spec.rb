5c75a2e2b79a0658a177ddd19cd5c7d9a1b2c3bf
Mariaaaa <mmmasha555@mail.ru>
2014-05-23 21:26:11 +0400

    12

---
+++ spec/models/study/exam_formreader_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Study::ExamFormreader do
+  it 'должен обладать валидной фабрикой' do
+    build(:exam_formreader).should be_valid
+  end
+
+  describe 'обладает связями с другими моделями:' do
+    it 'с экзаменом' do
+      should belong_to(:exam)
+    end
+  end
+
+end
