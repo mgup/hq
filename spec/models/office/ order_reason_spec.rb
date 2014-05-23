6a607f670689cbc28b6691daaafa788845ddf93e
nutav <vau2007@rambler.ru>
2014-05-23 23:24:38 +0400

    Update  order_reason_spec.rb

--- spec/models/office/ order_reason_spec.rb
+++ spec/models/office/ order_reason_spec.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
 
 describe Office::OrderReason do
-	it 'должен обладать валидной фабрикой' do
-		build(:position).should be_valid
-	end
+  it 'должен обладать валидной фабрикой' do
+    build(:position).should be_valid
+  end
 
-	describe 'обладает связями с другими моделями' do
-		it 'с порядком' do
-			should belong_to(:order)
-		end
+  describe 'обладает связями с другими моделями:' do
+    it 'с приказом' do
+      should belong_to(:order)
+    end
 
-		it 'с причинами' do
-			should belong_to(:reason)
-		end
-
-	end
+    it 'с основанием для приказа' do
+      should belong_to(:reason)
+    end
+  end
 end
