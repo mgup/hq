650a240f448fbec1d905f3fed8a42168964c0dc6
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-23 15:32:07 +0400

    Update position_spec.rb

--- spec/models/position_spec.rb
+++ spec/models/position_spec.rb
@@ -1,25 +1,22 @@
 require 'spec_helper'
 
 describe Position do
-  it 'должен обладать валидной фабрикой' do
-    build(:acl_position).should be_valid
-  end
-
+  
   describe 'обладает связями с другими моделями:' do
     it 'с событиями' do
-      should belongs_to(:user)
+      should belong_to(:user)
     end
     
     it 'с ролью' do
-      should belongs_to(:role)
+      should belong_to(:role)
     end
     
     it 'с отделом' do
-      should belongs_to(:department)
+      should belong_to(:department)
     end
     
     it 'с назначением' do
-      should belongs_to(:appointment)
+      should belong_to(:appointment)
     end
   end
 end
