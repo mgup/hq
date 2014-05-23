9de1f85b908937c7ba29835e66b3b3a007802adb
Mariaaaa <mmmasha555@mail.ru>
2014-05-23 21:54:14 +0300

    Update salary201403_spec.rb

--- spec/models/salary/salary201403_spec.rb
+++ spec/models/salary/salary201403_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe Salary::Salary201403 do
-  it 'должен обладать валидной фабрикой' do
-    build(:salary201403).should be_valid
-  end
-
+  
   describe 'обладает связями с другими моделями:' do
     it 'с пользователем' do
       should belong_to(:user)
