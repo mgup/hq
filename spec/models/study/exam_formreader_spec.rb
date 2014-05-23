2ed3ea79639b05dca04b0a58e2b0af7bf0fb9aac
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-23 17:50:13 +0400

    Update exam_formreader_spec.rb

--- spec/models/study/exam_formreader_spec.rb
+++ spec/models/study/exam_formreader_spec.rb
@@ -1,10 +1,7 @@
 require 'spec_helper'
 
 describe Study::ExamFormreader do
- 	it 'должен обладать валидной фабрикой' do
-		build(:exam_formreader).should be_valid
-	end
-
+ 	
     describe 'обладает связями с другими моделями:' do
  	    it 'с экзаменом' do
  	      should belong_to(:exam)
