c0ceed17ab5637a6f31a5cd70862f4dfe06ef5d3
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-23 16:51:53 +0400

    Проверка VisitorEventDate

---
+++ spec/models/visitor_event_date_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe VisitorEventDate do
+  it 'должен обладать валидной фабрикой' do
+    build(:visitor_event_date).should be_valid
+  end
+
+  describe 'обладает связями с другими моделями:' do
+    it 'с датой' do
+      should belong_to(:date)
+    end
+
+     it 'с посетителем' do
+      should belong_to(:visitor)
+    end
+
+    it 'с пользователем' do
+      should belong_to(:user)
+    end
+
+     it 'со студентом' do
+      should belong_to(:student)
+    end
+  end
+end
