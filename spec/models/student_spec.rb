3fea44eae7f9a502e58f1a3fe071f567ba19ee16
nutav <vau2007@rambler.ru>
2014-05-23 22:48:22 +0400

    Update student_spec.rb

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,7 +6,7 @@ describe Student do
 	end
 
 	describe 'обладает связями с другими моделями:' do
-		it 'с именем студента' do
+		it 'с человеком' do
 			should belong_to(:person).class_name('Person')
 		end
 
@@ -14,7 +14,7 @@ describe Student do
 			should belong_to(:group).class_name('Group')
 		end
 
-		it 'с оценкой' do
+		it 'с оценками' do
 			should have_many(:marks).class_name('Study::Mark')
 		end
 
@@ -22,43 +22,52 @@ describe Student do
 			should have_many(:exams).class_name('Study::Exam')
 		end
 
-		it 'с контрольными точками' do
+		it 'с оценками (сессии)' do
 			should have_many(:xmarks).class_name('Study::Xmark')
 		end
-		it 'с документами студента' do
+		
+		it 'с таблицей, обеспечивающей связь с документами' do
 			should have_many(:document_students).class_name('Document::DocumentStudent')
 		end
-		it 'с зачетной книжкой' do
-			should have_many(:documents).class_name('Document::Doc')
+		
+		it 'с документами' do
+			should have_many(:documents).class_name('Document::Doc').through(:document_students)
 		end
+		
 		it 'с платежами' do
 			should have_many(:payments).class_name('Finance::Payment')
 		end
-		it 'с поддержкой' do
+		
+		it 'с заявлениями на материальную помощь' do
 			should have_many(:supports).class_name('My::Support')
 		end
 		
-		it 'с выбором специальности' do
+		it 'с таблицей, обеспечивающей связь с предметами по выбору' do
 			should have_many(:selections).class_name('My::Select')
 		end
-		it 'с выбором оценки' do
-			should have_many(:choices).class_name('My::Choice')
+		
+		it 'с предметами по выбору' do
+			should have_many(:choices).class_name('My::Choice').through(:selections)
 		end
-		it 'с порядком студента' do
+		
+		it 'с таблицей, обеспечивающей связь с приказами' do
 			should have_many(:students_in_order).class_name('Office::OrderStudent')
 		end
 		
-		it 'с порядком' do
-			should have_many(:orders).class_name('Office::Order')
+		it 'с приказами' do
+			should have_many(:orders).class_name('Office::Order').through(:students_in_order)
 		end
-		it 'с посетителями мероприятия' do
-			should have_many(:visitor_event_dates)
+		
+		it 'с таблицей, обеспечивающей связь с датами мероприятий' do
+			should have_many(:visitor_event_dates).class_name('VisitorEventDate')
 		end
-		it 'с датой' do
-			should have_many(:dates).through(:visitor_event_dates)
+		
+		it 'с датами мероприятий' do
+			should have_many(:dates).class_name('EventDate').through(:visitor_event_dates)
 		end
-	it 'со студентом, имеющим ученую степень' do
-			should have_many(:graduate_student)
+		
+		it 'с таблицей, обеспечивающей связь с дипломом' do
+			should have_one(:graduate_student)
 		end
 	end
 end
