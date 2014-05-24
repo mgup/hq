56adb96839bb34c4295e8c7efd63a62552ed6eed
SvetlanaB <svetik5463@list.ru>
2014-05-24 14:00:34 +0400

    2

--- spec/models/curator/task_spec.rb
+++ spec/models/curator/task_spec.rb
@@ -1,23 +1,19 @@
 ﻿require 'spec_helper'
 
 describe Curator::Task do
-
 	it 'должен обладать валидной фабрикой' do
 		build(:curator_task).should be_valid
 	end
 
-	describe 'обладает связями с другими моделями' do 
-
-		it 'с задачами пользователей' do
-			should have_many(:task_users)
-		end
-
-		it 'с пользователями' do
-			should have_many(:users).through(:task_users)
-		end
-
-		it 'с типом' do
+	describe 'обладает связями с другими моделями:' do 
+		it 'с типом задания' do
 			should belong_to(:type)
 		end
+		it 'с индивидуальным заданием' do
+			should have_many(:task_users)
+		end
+		it 'с пользователем' do
+			should have_many(:users)
+		end		
     end
 end
