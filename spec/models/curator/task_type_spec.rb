b454af96cb351cde43d30f149ccd19dc691f166f
SvetlanaB <svetik5463@list.ru>
2014-05-26 16:49:53 +0400

    task_type_spec

---
+++ spec/models/curator/task_type_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Curator::TaskType do
+
+	describe 'обладает связями с другими моделями:' do 	
+		it 'с заданиями' do
+			should have_many(:tasks)
+		end		
+	end	
+
+	describe 'обладает ограничениями на поля' do
+		it 'обязательное поле присутствия имени' do	
+			should validate_presence_of(:name)
+		end
+    end
+end
