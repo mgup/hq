37a9580d81ea99758c1c7a8aba86db6e8dd4832a
Aleeena <alena.mgup@yandex.ru>
2014-05-26 19:21:46 +0400

    graduate_student model
    ЛР №3 Логинова Алена

---
+++ spec/models/graduate_student_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe GraduateStudent do
+
+  describe 'обладает связями с другими моделями:' do
+    it 'с Graduate' do
+      should belong_to(:graduate)
+    end
+    it 'со студентом' do
+      should belong_to(:student)
+    end
+    it 'с выпускными оценками' do
+      should have_many(:graduate_marks)
+    end
+  end
+
+end
