0b10fb824766d0a35779026ad087c1c0055a426f
SvetlanaB <svetik5463@list.ru>
2014-05-26 20:24:55 +0400

    11

--- spec/models/curator/task_type_spec.rb
+++ spec/models/curator/task_type_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Curator::TaskType do
 
 	describe 'обладает ограничениями на поля' do
-		it 'обязательное поле присутствия имени' do	
+		it 'обязательное поле присутствия названия' do	
 			should validate_presence_of(:name)
 		end
     end
--- spec/models/finance/price_spec.rb
+++ spec/models/finance/price_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Finance::Price do
 	describe 'обладает связями с другими моделями:' do 
-		it 'с типом' do
+		it 'с типом оплаты' do
 			should belong_to(:type)
 		end
     end
