1dda32b62019f5dcc0d82cf6b81a951a1f261bf8
Светлана Базарова <svetik5463@list.ru>
2014-05-27 17:44:44 +0400

    Update price_spec.rb

--- spec/models/finance/price_spec.rb
+++ spec/models/finance/price_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Finance::Price do
-	describe 'обладает связями с другими моделями:' do 
-		it 'с типом оплаты' do
-			should belong_to(:type)
-		end
+  describe 'обладает связями с другими моделями:' do 
+    it 'с типом оплаты' do
+      should belong_to(:type)
     end
+  end
 end
