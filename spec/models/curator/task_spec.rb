f3b35fc7e839e12fd59d5029e8ca7497848a291a
Светлана Базарова <svetik5463@list.ru>
2014-05-27 06:34:26 -0700

    Update task_spec.rb

--- spec/models/curator/task_spec.rb
+++ spec/models/curator/task_spec.rb
@@ -1,29 +1,26 @@
-﻿require 'spec_helper'
-
+require 'spec_helper'
 describe Curator::Task do
-
-	describe 'обладает связями с другими моделями:' do 
-		
-		it 'с типом задания' do
-			should belong_to(:type)
-		end
-		it 'с индивидуальным заданием' do
-			should have_many(:task_users)
-		end
-		it 'с пользователями' do
-			should have_many(:users)
-		end		
+  describe 'обладает связями с другими моделями:' do 
+    it 'с типом задания' do
+      should belong_to(:type)
     end
-
-    describe 'обладает ограничениями на поля' do
-		it 'обязательное поле присутствия названия' do	
-			should validate_presence_of(:name)
-		end
-		it 'обязательное поле присутствия статуса' do
-			should validate_presence_of(:status)
-		end
-		it 'обязательное поле присутствия типа' do
-			should validate_presence_of(:type)
-		end
+    it 'с индивидуальным заданием' do
+      should have_many(:task_users)
     end
+    it 'с пользователями' do
+      should have_many(:users)
+    end		
+  end
+
+  describe 'обладает ограничениями на поля' do
+    it 'обязательное поле присутствия названия' do	
+      should validate_presence_of(:name)
+    end
+    it 'обязательное поле присутствия статуса' do
+      should validate_presence_of(:status)
+    end
+    it 'обязательное поле присутствия типа' do
+      should validate_presence_of(:type)
+    end
+  end
 end
