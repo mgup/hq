d23cd2b0956944b2489babf1a016b46570e72547
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-27 16:16:35 +0300

    Update exam_formreader_spec.rb

--- spec/models/study/exam_formreader_spec.rb
+++ spec/models/study/exam_formreader_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
-
 describe Study::ExamFormreader do
- 	
-    describe 'обладает связями с другими моделями:' do
- 	    it 'с экзаменом' do
- 	      should belong_to(:exam)
- 	    end
- 	end
- end
+describe 'обладает связями с другими моделями:' do
+  it 'с экзаменом' do
+  should belong_to(:exam)
+  end
+end
+end
