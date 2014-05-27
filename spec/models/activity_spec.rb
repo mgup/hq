6f3ae4296b3b8fb3cae1515e91ac4dd0567be21d
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-27 16:18:03 +0300

    Update activity_spec.rb

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,25 +1,20 @@
 require 'spec_helper'
 describe Activity do
-	
-	describe 'обладает связями с другими моделями:' do
-	    it 'с активной группой' do
-	      should belong_to(:activity_group)
-	    end
-
-	    it 'с группой' do
-	      should belong_to(:group)
-	    end
-
-	  it 'с активным типом' do
-	      should belong_to(:activity_type)
-	    end
-
-	    it 'с активным кредитным типом' do
-	      should belong_to(:activity_credit_type)
-	    end
-
-  		it 'с достижениями' do
-	      should have_many(:achievements)
-	    end
-	end
+describe 'обладает связями с другими моделями:' do
+  it 'с активной группой' do
+  should belong_to(:activity_group)
+  end
+  it 'с группой' do
+  should belong_to(:group)
+  end
+  it 'с активным типом' do
+  should belong_to(:activity_type)
+  end
+  it 'с активным кредитным типом' do
+  should belong_to(:activity_credit_type)
+  end
+  it 'с достижениями' do
+  should have_many(:achievements)
+  end
+end
 end
