cda00aeb2229962ab3c1a2a7f0f27c4a4e1077c1
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-27 16:32:57 +0300

    Update  group_spec.rb

--- spec/models/curator/ group_spec.rb
+++ spec/models/curator/ group_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
+
 describe Curator::Group do
 
-	describe 'обладает связями с другими моделями:' do
-	    it 'с куратором' do
-	      should belong_to(:curator)
-	    end
+  describe 'обладает связями с другими моделями:' do
+    it 'с куратором' do
+      should belong_to(:curator)
+    end
 
-	    it 'с группой' do
-	      should belong_to(:group)
-	    end
-	end
+    it 'с группой' do
+      should belong_to(:group)
+    end
+    
+  end
 end
+
