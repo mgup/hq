8a8e120e8269a2cb6fe394ebd4ebe86caf299909
Светлана Базарова <svetik5463@list.ru>
2014-05-27 17:40:49 +0400

    Update task_user_spec.rb

--- spec/models/curator/task_user_spec.rb
+++ spec/models/curator/task_user_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe Curator::TaskUser do
-
-	describe 'обладает связями с другими моделями:' do 	
-		it 'с заданием' do
-			should belong_to(:task)
-		end		
-		it 'с пользователем' do
-			should belong_to(:user)
-		end
-	end	
+  describe 'обладает связями с другими моделями:' do 	
+    it 'с заданием' do
+      should belong_to(:task)
+    end		
+    it 'с пользователем' do
+      should belong_to(:user)
+    end
+  end	
 end
