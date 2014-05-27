b53f25dee48d98a73425ee6ce0f8109bc51d35c9
Mariaaaa <mmmasha555@mail.ru>
2014-05-27 17:44:07 +0400

    Update salary201403_spec.rb

--- spec/models/salary/salary201403_spec.rb
+++ spec/models/salary/salary201403_spec.rb
@@ -5,9 +5,11 @@ describe Salary::Salary201403 do
     it 'с пользователем' do
       should belong_to(:user)
     end
+    
     it 'с департаментом' do
       should belong_to(:department)
     end
+    
   end
 end
 
