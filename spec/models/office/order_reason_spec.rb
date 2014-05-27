543166641f466af968dd89672cc0dd0170b11cbd
Maria Pinyazhina <mary_pin@mail.ru>
2014-05-27 16:20:38 +0300

    Update order_reason_spec.rb

--- spec/models/office/order_reason_spec.rb
+++ spec/models/office/order_reason_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
-
 describe Office::OrderReason do
-	
-	describe 'обладает связями с другими моделями' do
-		it 'с порядком' do
-			should belong_to(:order)
-		end
-		
-		it 'с причиной' do
-			should belong_to(:reason)
-		end
-	end
+  describe 'обладает связями с другими моделями' do
+    it 'с порядком' do
+      should belong_to(:order)
+    end
+    it 'с причиной' do
+      should belong_to(:reason)
+    end
+  end
 end
