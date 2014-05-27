a67c08b58f1bf244e12dd70f6bbb7bbb723a83fa
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-27 16:24:15 +0300

    Update graduate_spec.rb

--- spec/models/graduate_spec.rb
+++ spec/models/graduate_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
+
 describe Graduate do
   
   describe 'обладает связями с другими моделями:' do
-
     it 'с групой' do
       should belong_to(:group)
     end
-
+      
     it 'с выпускными предметами' do
       should have_many(:graduate_subjects)
     end
@@ -14,5 +14,6 @@ describe Graduate do
     it 'с аспирантами' do
       should have_many(:graduate_students)
     end
+    
   end
 end
