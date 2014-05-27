0a92d4d8f0a719c6b9acdecd9b4f677fe956d3c6
Bato Shoybonow <batoshoi@mail.ru>
2014-05-27 17:23:27 +0400

    Update activity_spec.rb

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,38 +1,32 @@
 require 'spec_helper'
 describe Activity do
-
-	describe 'обладает ограничениями на поля' do
-
-		it 'обязательное поле присутствия названия' do 
-			should validate_presence_of(:name)
-		end
-		it 'обязательное поле присутствия id активной группы' do 
-			should validate_presence_of(:activity_group_id)
-		end
-		it 'обязательное поле присутствия id активного типа' do 
-			should validate_presence_of(:activity_type_id)
-		end
-	end 	
+  describe 'обладает ограничениями на поля' do
+    it 'обязательное поле присутствия названия' do 
+      should validate_presence_of(:name)
+    end
+    it 'обязательное поле присутствия id активной группы' do 
+      should validate_presence_of(:activity_group_id)
+    end
+    it 'обязательное поле присутствия id активного типа' do 
+      should validate_presence_of(:activity_type_id)
+    end	
+  end 	
 	
-	describe 'обладает связями с другими моделями:' do
-
-	    it 'с активной группой' do
-	      should belong_to(:activity_group)
-	    end
-	    it 'с группой' do
-	      should belong_to(:group)
-	    end
-	  	it 'с активным типом' do
-	      should belong_to(:activity_type)
-	    end
-	    it 'с активным кредитным типом' do
-	      should belong_to(:activity_credit_type)
-	    end
-	    #it 'с валидатором' do
-	    #  should belong_to(:validator)
-	    #end pochemu to ne rabotaet:(
-	  	it 'с достижениями' do
-	      should have_many(:achievements)
-	    end
-	end
+  describe 'обладает связями с другими моделями:' do
+    it 'с активной группой' do
+      should belong_to(:activity_group)
+    end
+    it 'с группой' do
+      should belong_to(:group)
+    end
+    it 'с активным типом' do
+      should belong_to(:activity_type)
+    end
+    it 'с активным кредитным типом' do
+      should belong_to(:activity_credit_type)
+    end
+    it 'с достижениями' do
+      should have_many(:achievements)
+    end
+  end
 end
