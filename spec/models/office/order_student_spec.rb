6168d3a649300aae97e4ae7748fbfd29708a4640
Maria Pinyazhina <mary_pin@mail.ru>
2014-05-27 16:40:42 +0300

    Update order_student_spec.rb

--- spec/models/office/order_student_spec.rb
+++ spec/models/office/order_student_spec.rb
@@ -1,16 +1,12 @@
 require 'spec_helper'
-
 describe Office::OrderStudent do
-  
   describe 'обладает связями с другими моделями' do
     it 'с порядком' do
       should belong_to(:order)
     end
-    
     it 'со студентом' do
       should belong_to(:student)
     end
-
   end
 end
 
