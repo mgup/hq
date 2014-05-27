bc9afea9f86b9f831ec6da070bff6c981813f3ff
Светлана Базарова <svetik5463@list.ru>
2014-05-27 17:51:09 +0400

    Update payment_spec.rb

--- spec/models/finance/payment_spec.rb
+++ spec/models/finance/payment_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe Finance::Payment do
-	describe 'обладает связями с другими моделями:' do 
-		it 'со студентом' do
-			should belong_to(:student)
-		end
+  describe 'обладает связями с другими моделями:' do 
+    it 'со студентом' do
+      should belong_to(:student)
     end
+  end
 end
