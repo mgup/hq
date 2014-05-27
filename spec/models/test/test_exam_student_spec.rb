0a7368e6ac7192b8c38557a2ad19c3af20cafa3d
Bato Shoybonow <batoshoi@mail.ru>
2014-05-27 17:26:08 +0400

    Update test_exam_student_spec.rb

--- spec/models/test/test_exam_student_spec.rb
+++ spec/models/test/test_exam_student_spec.rb
@@ -1,12 +1,1 @@
 require 'spec_helper'
-# describe  ExamStudent do
-#   describe 'обладает связями с другими моделями:' do
-#       it 'с экзаменом' do
-#         should belong_to(:exam)
-#       end
-
-#       it 'со студентом' do
-#         should belong_to(:student)
-#       end
-#   end
-# end
