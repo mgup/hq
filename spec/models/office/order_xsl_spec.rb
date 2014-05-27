90c43569d69a95f3a974340cf9a30606e791f63f
Maria Pinyazhina <mary_pin@mail.ru>
2014-05-27 16:41:27 +0300

    Update order_xsl_spec.rb

--- spec/models/office/order_xsl_spec.rb
+++ spec/models/office/order_xsl_spec.rb
@@ -1,12 +1,9 @@
 require 'spec_helper'
-
 describe Office::OrderXsl do
-  
   describe 'обладает связями с другими моделями' do
     it 'с порядком шаблона' do
       should belong_to(:order_template)
     end
-    
   end
 end
 
