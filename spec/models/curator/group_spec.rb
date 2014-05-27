3ec1a8580e0689b291de8caa5a2db0fd813bf2c3
Светлана Базарова <svetik5463@list.ru>
2014-05-27 06:30:02 -0700

    Update group_spec.rb

--- spec/models/curator/group_spec.rb
+++ spec/models/curator/group_spec.rb
@@ -1,13 +1,11 @@
 require 'spec_helper'
-
 describe Curator::Group do
-
-	describe 'обладает связями с другими моделями:' do 	
-		it 'с куратором' do
-			should belong_to(:curator)
-		end		
-		it 'с группой' do
-			should belong_to(:group)
-		end
-	end	
+  describe 'обладает связями с другими моделями:' do 	
+    it 'с куратором' do
+       should belong_to(:curator)
+    end		
+    it 'с группой' do
+      should belong_to(:group)
+    end
+  end	
 end
