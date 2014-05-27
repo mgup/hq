4acfee5e22c94338540af09f390dc5a351cb09ce
Bato Shoybonow <batoshoi@mail.ru>
2014-05-27 17:46:16 +0400

    Update activity_spec.rb

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -5,9 +5,11 @@ describe Activity do
     it 'обязательное поле присутствия названия' do 
       should validate_presence_of(:name)
     end
+    
     it 'обязательное поле присутствия id активной группы' do 
       should validate_presence_of(:activity_group_id)
     end
+    
     it 'обязательное поле присутствия id активного типа' do 
       should validate_presence_of(:activity_type_id)
     end	
@@ -16,15 +18,19 @@ describe Activity do
     it 'с активной группой' do
       should belong_to(:activity_group)
     end
+    
     it 'с группой' do
       should belong_to(:group)
     end
+    
     it 'с активным типом' do
       should belong_to(:activity_type)
     end
+    
     it 'с активным кредитным типом' do
       should belong_to(:activity_credit_type)
     end
+    
     it 'с достижениями' do
       should have_many(:achievements)
     end
