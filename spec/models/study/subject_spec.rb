d0860c01af65d3923fb1c62857429dd5fa6a4ce8
Mariaaaa <mmmasha555@mail.ru>
2014-05-27 17:44:35 +0400

    Update subject_spec.rb

--- spec/models/study/subject_spec.rb
+++ spec/models/study/subject_spec.rb
@@ -5,12 +5,15 @@ describe Study::Subject do
     it 'с группой' do
       should belong_to(:group)
     end
+    
         it 'с пользователями' do
       should belong_to(:user)
     end
+    
     it 'с оценками' do
       should have_many(:marks)
     end
+    
   end
 end
 
