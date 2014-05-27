330625159ba8b9f818052bc5769f8e6d5de58879
Maria Pinyazhina <mary_pin@mail.ru>
2014-05-27 16:54:46 +0300

    Update event_date_spec.rb

--- spec/models/event_date_spec.rb
+++ spec/models/event_date_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
+
 describe EventDate do
   describe 'обладает связями с другими моделями' do
     it 'с событием' do
       should belong_to(:event)
     end
+    
     it 'пользователей' do
       should have_many(:users)
     end
+    
     it 'студентов' do
       should have_many(:students)
     end
