312fa1d0d0eeae38924a8c77bacaa72b232eaf5b
Mariaaaa <mmmasha555@mail.ru>
2014-05-27 17:49:24 +0400

    Update subject_spec.rb

--- spec/models/study/subject_spec.rb
+++ spec/models/study/subject_spec.rb
@@ -6,14 +6,13 @@ describe Study::Subject do
       should belong_to(:group)
     end
     
-        it 'с пользователями' do
+    it 'с пользователями' do
       should belong_to(:user)
     end
     
     it 'с оценками' do
       should have_many(:marks)
     end
-    
   end
 end
 
