ef0e4555115625ada9030fc15c74e91f7a5e3720
Bato Shoybonow <batoshoi@mail.ru>
2014-05-27 17:24:52 +0400

    Update document_student_spec.rb

--- spec/models/document/document_student_spec.rb
+++ spec/models/document/document_student_spec.rb
@@ -1,14 +1,11 @@
 require 'spec_helper'
 describe Document::DocumentStudent do
-	# it 'должен обладать валидной фабрикой' do
-	# 	build(:documentstudent).should be_valid
-	# end
-	describe 'обладает связями с другими моделями:' do
-	    it 'с документами' do
-	      should belong_to(:document)
-	    end
-	    it 'со студентом' do
-	      should belong_to(:student)
-	    end
-	end
+  describe 'обладает связями с другими моделями:' do
+    it 'с документами' do
+      should belong_to(:document)
+    end
+    it 'со студентом' do
+      should belong_to(:student)
+    end
+  end
 end
