251a29d8d4019abb2988e0e82035f9c0b07f2761
Maria Pinyazhina <mary_pin@mail.ru>
2014-05-27 16:53:58 +0300

    Update reason_spec.rb

--- spec/models/office/reason_spec.rb
+++ spec/models/office/reason_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
+
 describe Office::Reason do
   describe 'обладает связями с другими моделями' do
     it 'с шаблоном' do
       should belong_to(:template)
     end
+
     it 'видов причин' do
       should have_many(:order_reasons)
     end
+
     it 'видов' do
       should have_many(:orders)
     end
