be45e72d1bb3ad9e6cd841683df78ca5b97ef393
HelenHoneyCake <ogoltsova2008@mail.ru>
2014-05-27 15:38:37 -0700

    Update activity_group_spec.rb

--- spec/models/activity_group_spec.rb
+++ spec/models/activity_group_spec.rb
@@ -1,15 +1,13 @@
 require 'spec_helper'
 
 describe ActivityGroup do
-#  describe 'обладает ограничениями на поля' do
-#    it 'обязательно присутствие названия' do
-#      should validate_presense_of(:name)
-#    end
-#  end
-  
   describe 'обладает связями' do
     it 'с активностью' do
       should have_many(:activities)
+    end
+    
+    it 'обязательно присутствие названия' do
+      should validate_presence_of(:name)
     end
   end
   
