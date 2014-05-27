ca33922a25efa6515d1648446dec04801b147093
SvetlanaSmot <svetlana.smot@gmail.com>
2014-05-27 16:31:51 +0300

    Update payment_type_spec.rb

--- spec/models/finance/payment_type_spec.rb
+++ spec/models/finance/payment_type_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
+
 describe Finance::PaymentType do
 
-	describe 'обладает связями с другими моделями:' do
-	    it 'с ценой' do
-	      should have_many(:prices)
-	    end
+  describe 'обладает связями с другими моделями:' do
+    it 'с ценой' do
+      should have_many(:prices)
+    end
 
-	    it 'с специальностью' do
-	      should belong_to(:speciality)
-	    end
-	end
+    it 'с специальностью' do
+      should belong_to(:speciality)
+    end
+    
+  end
 end
+
