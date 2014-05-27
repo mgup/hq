3e22b1b0913ea41735bd21cb620244713f0ef4a9
Bato Shoybonow <batoshoi@mail.ru>
2014-05-27 17:25:51 +0400

    Update exam_student_spec.rb

--- spec/models/study/exam_student_spec.rb
+++ spec/models/study/exam_student_spec.rb
@@ -4,11 +4,9 @@ describe Study::ExamStudent do
     it 'с экзаменом' do
       should belong_to(:exam)
     end
-
     it 'со студентом' do
       should belong_to(:student)
     end
-
     it 'с персоной' do
       should belong_to(:person)
     end
