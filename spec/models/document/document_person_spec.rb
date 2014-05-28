a82d426667a6b8a45e6cc4a833f77a4b0ac52e23
HelenHoneyCake <ogoltsova2008@mail.ru>
2014-05-28 10:54:50 -0700

    Update document_person_spec.rb

--- spec/models/document/document_person_spec.rb
+++ spec/models/document/document_person_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Document::DocumentPerson do
-  it 'должен обладать валидной фабрикой' do
-    build(:document_student).should be_valid
-  end
-
   describe 'должен обладать связями с:' do
     it 'документами' do
       should belong_to(:document).class_name('Document::Doc')
