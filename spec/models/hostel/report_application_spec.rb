29cd83b67c4142ad503b3f6f001af89dc9d476da
Oleg <Z4ckylol@gmail.com>
2014-05-28 17:38:13 +0300

    Update report_application_spec.rb

--- spec/models/hostel/report_application_spec.rb
+++ spec/models/hostel/report_application_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Hostel::ReportApplication do
   describe 'обладает связями с другими моделями:' do
     it 'с отчетом' do
-      should belong_to(Hostel::Report)
+        should belong_to(:report)
     end
   end
 end
