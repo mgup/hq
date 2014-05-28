ddf337e0dbf63e261c55cd4bf2a924b2012441c6
Aleeena <alena.mgup@yandex.ru>
2014-05-28 15:16:31 +0400

    graduate_mark_spec FINAL

--- spec/models/graduate_mark_spec.rb
+++ spec/models/graduate_mark_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe GraduateMark do
-  describe 'обладает связями с другими моделями:' do
-    it 'со студентом-выпускником' do
-      should belong_to(:graduate_student)
-    end
-    it 'со graduate_subject' do
-      should belong_to(:graduate_subject)
-    end
-  end
-
+	describe 'обладает связями с другими моделями:' do
+		it 'со студентом-выпускником' do
+			should belong_to(:graduate_student)
+		end
+		it 'со graduate_subject' do
+			should belong_to(:graduate_subject)
+		end
+	end
 end
