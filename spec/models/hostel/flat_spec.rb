f711b8b7eb39f231368bea00d9f63aef0d009a16
HelenHoneyCake <ogoltsova2008@mail.ru>
2014-05-28 08:39:46 -0700

    Create flat_spec.rb

---
+++ spec/models/hostel/flat_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe Hostel::Flat do
+  describe 'обладает связями с другими моделями:' do 	
+    it 'с общежитием' do
+      should belong_to(:hostel).class_name('Hostel::Host')
+    end
+    
+    it 'с комнатами' do
+      should have_many(:rooms).class_name('Hostel::Room')
+    end
+    
+    it 'с проживающими' do
+      should have_many(:residents).class_name('Person')
+    end
+  end
+end
