28bbdd5a61d5298137aa65d784a25b9e49ce3c25
HelenHoneyCake <ogoltsova2008@mail.ru>
2014-05-28 10:22:39 -0700

    Update doc_spec.rb

--- spec/models/document/doc_spec.rb
+++ spec/models/document/doc_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe Document::Doc do
   describe 'должен обладать связями с:' do
-    it 'документами студентов' do
-      should have_many(:document_students).class_name('Document::DocumentStudent')
-    end
-
     it 'студентами' do
       should have_many(:students)
     end
