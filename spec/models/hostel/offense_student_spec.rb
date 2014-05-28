6ce9cf92f145751e0a36bf5c3fc32a4f5936e701
Aleeena <alena.mgup@yandex.ru>
2014-05-28 16:16:50 +0400

    offense_student_spec FINAL

--- spec/models/hostel/offense_student_spec.rb
+++ spec/models/hostel/offense_student_spec.rb
@@ -1,13 +1,7 @@
 require 'spec_helper'
 
 describe Hostel::OffenseStudent do
-	it 'должен обладать валидной фабрикой' do
-		build(:hostel_report_offense_student).should be_valid
-	end
 	describe 'обладает связями с другими моделями:' do
-		it 'с правонарушением' do
-			should belong_to(:offense).class_name('Hostel::ReportOffense')
-		end
 		it 'с человеком' do
 			should belong_to(:person).class_name('Person')
 		end
