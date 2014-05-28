0cdaf142ae9473c666258519bd0682deaa20c68a
Aleeena <alena.mgup@yandex.ru>
2014-05-28 12:13:15 +0400

    offence_room_spec2

--- spec/models/hostel/offence_room_spec.rb
+++ spec/models/hostel/offence_room_spec.rb
@@ -1,13 +1,10 @@
 require 'spec_helper'
 
 describe Hostel::OffenseRoom do
-  it 'должен обладать валидной фабрикой' do
-    build(:hostel_report_offense_room).should be_valid
-  end
 
   describe 'обладает связями с другими моделями:' do
     it 'с правонарушением' do
-      should belong_to(:offence).class_name('Hostel::ReportOffense')
+      should belong_to(:offence)
     end
 
     it 'с комнатой' do
