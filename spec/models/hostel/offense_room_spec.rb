531aa49d54bb65758936b3f0825fbcac340da609
Aleeena <alena.mgup@yandex.ru>
2014-05-28 13:09:20 +0400

    1111

--- spec/models/hostel/offense_room_spec.rb
+++ spec/models/hostel/offense_room_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 describe Hostel::OffenseRoom do
 
   describe 'обладает связями с другими моделями:' do
-    it 'с правонарушением' do
-      should belong_to(:offense)
-    end
-
     it 'с комнатой' do
       should belong_to(:room).class_name('Hostel::Room')
     end
