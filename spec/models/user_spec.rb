c5ee643d01cda1b56fef1b14b6a8264139f8af89
HelenHoneyCake <ogoltsova2008@mail.ru>
2014-05-28 14:22:07 -0700

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,5 +41,25 @@ describe User do
     it 'с датами' do
       should have_many(:dates).through(:visitor_event_dates)
     end
+    
+    it 'с задачами' do
+      should have_many(:tasks).through(:task_users)
+    end
+    
+    it 'с пользователями задач' do
+      should have_many(:task_users)
+    end
+    
+    it 'с группами кураторов' do
+      should have_many(:curator_groups).class_name('Curator::Group')
+    end
+    
+    it 'с группами' do
+      should have_many(:groups).through(:curator_groups)
+    end
+    
+    it 'с текущими группами' do
+      should have_many(:current_groups)
+    end
   end
 end
