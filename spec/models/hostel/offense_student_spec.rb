01d27c9efe3a679cd909ea5a6584c87c19494132
Aleeena <alena.mgup@yandex.ru>
2014-05-28 16:35:24 +0400

    delete offense_student_spec

--- spec/models/hostel/offense_student_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Hostel::OffenseStudent do
-	describe 'обладает связями с другими моделями:' do
-		it 'с человеком' do
-			should belong_to(:person).class_name('Person')
-		end
-	end
-end
