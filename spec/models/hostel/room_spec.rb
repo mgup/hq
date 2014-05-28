84abcfa562553b74bac132b4db1f1826e7f7c73b
staygold48 <staygold48@gmail.com>
2014-05-28 14:47:54 +0300

    Hostel::Room

---
+++ spec/models/hostel/room_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Hostel::Room do
+  describe 'обладает связями с другими моделями:' do 	
+    it 'с квартирой' do
+      should belong_to(:flat)
+    end
+    
+    it 'с жителями' do
+      should have_many(:residents)
+    end	
+  end
+end
