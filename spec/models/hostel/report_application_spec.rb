20a5b25cac0483ea22cda6e527e00de9b34bb5cf
staygold48 <staygold48@gmail.com>
2014-05-28 14:45:16 +0300

    Hostel::ReportApplication

---
+++ spec/models/hostel/report_application_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Hostel::ReportApplication do
+  describe 'обладает связями с другими моделями:' do 	
+    it 'с отчетом' do
+      should belong_to(:report)
+    end		
+  end
+end
