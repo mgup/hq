4ea93d63f58b785f7171e1be9bf6fbeec98e9243
Aleeena <alena.mgup@yandex.ru>
2014-05-28 14:45:40 +0400

    graduate_student_spec

--- spec/models/graduate_student_spec.rb
+++ spec/models/graduate_student_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe GraduateStudent do
-
   describe 'обладает связями с другими моделями:' do
     it 'с Graduate' do
       should belong_to(:graduate)
@@ -13,6 +12,4 @@ describe GraduateStudent do
       should have_many(:graduate_marks)
     end
   end
-
-
 end
