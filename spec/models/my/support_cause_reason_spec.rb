1facea9b086ce5151c70fe2a753d217f11c6c8f8
alisoldman <alisoldman@ya.ru>
2014-06-03 14:40:54 +0300

    My::SupportCauseReason

---
+++ spec/models/my/support_cause_reason_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe My::SupportCauseReason do
+  describe 'обладает связями с другими моделями:' do
+    it 'с причиной' do
+      should belong_to(:cause)
+    end
+    
+    it 'с причиной' do
+      should belong_to(:reason)
+    end
+  end
+end
