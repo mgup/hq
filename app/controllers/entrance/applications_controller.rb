da2cd0dcc0a9af56a241fd13ddf280d587529321
Vitaly Shlyaga <storkvist@storkvist.net>
2014-06-20 10:46:52 +0400

    выборка заявлений

--- app/controllers/entrance/applications_controller.rb
+++ app/controllers/entrance/applications_controller.rb
@@ -6,13 +6,22 @@ class Entrance::ApplicationsController < ApplicationController
   def index
     # Находим все подходящие конкурсные группы.
     entrant_exams = @entrant.exam_results.map { |r| r.exam_id }.sort
+    possible_exams = []
+    (1..entrant_exams.size).each do |n|
+      entrant_exams.combination(n).to_a.each do |combination|
+        possible_exams << combination.sort
+      end
+    end
+    possible_exams.uniq!
 
     Entrance::CompetitiveGroup.all.each do |g|
       needed_exams = g.test_items.map { |i| i.exam_id }
 
-      if entrant_exams == needed_exams.sort
-        # Эта конкурсная группа подходит.
-        @entrant.applications.build(competitive_group_item_id: g.items.first.id)
+      possible_exams.each do |combination|
+        if combination == needed_exams.sort
+          # Эта конкурсная группа подходит.
+          @entrant.applications.build(competitive_group_item_id: g.items.first.id)
+        end
       end
     end
   end
