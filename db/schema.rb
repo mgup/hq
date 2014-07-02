cbcbddcc95cdce3e4b3ca163065db6bbb5666e0b
Vitaly Shlyaga <storkvist@storkvist.net>
2014-07-02 11:08:54 +0400

    Дата отзыва заявления

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701071823) do
+ActiveRecord::Schema.define(version: 20140702065932) do
 
   create_table "achievement_periods", force: true do |t|
     t.integer  "year",                       null: false
