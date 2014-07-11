ba931d5716b4c7cf5b65d8a1863f4743c0f8a15b
Vitaly Shlyaga <storkvist@storkvist.net>
2014-07-11 15:24:01 +0400

    Тестирование ошибок

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < ActionController::Base
 
   unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: lambda { |exception|
+      notify_honeybadger(exception)
       render_error 500, exception
     }
     rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception|
