2e8402452b006fde02686e5da3f83de30c191619
Vitaly Shlyaga <storkvist@storkvist.net>
2014-07-11 18:01:13 +0400

    Косяки

--- app/controllers/entrance/campaigns_controller.rb
+++ app/controllers/entrance/campaigns_controller.rb
@@ -108,7 +108,7 @@ class Entrance::CampaignsController < ApplicationController
         doc = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
           xml.Applications do
             @applications.each do |application|
-              unless application.entrant.pseries.blank?
+              unless @applications.entrant.pnumber.blank?
                 xml << application.to_fis.xpath('/Application').to_xml.to_str
               end
             end
