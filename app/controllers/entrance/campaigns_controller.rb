5e1d07730185baa64951195623edaee7dddccd6a
Vitaly Shlyaga <storkvist@storkvist.net>
2014-08-08 16:42:24 +0400

    Закрытие рейтингов для неавторизованных пользователей.

--- app/controllers/entrance/campaigns_controller.rb
+++ app/controllers/entrance/campaigns_controller.rb
@@ -13,7 +13,11 @@ class Entrance::CampaignsController < ApplicationController
 
   # Пофамильные списки поступающих (рейтинги).
   def rating
-    @items = Entrance::CompetitiveGroupItem.find(@applications.collect{ |app| app.competitive_group_item_id }.uniq)
+    if user_signed_in?
+      @items = Entrance::CompetitiveGroupItem.find(@applications.collect{ |app| app.competitive_group_item_id }.uniq)
+    else
+      render 'entrance/campaigns/rating_hidden'
+    end
   end
 
   def crimea_rating
