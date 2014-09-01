1cc56d6dc4a2c534a8d1bfdbb480260f6e9c3f9b
Vitaly Shlyaga <storkvist@storkvist.net>
2014-09-01 14:07:26 +0400

    Попытка перебороть CanCan::AccessDenied в Honeybadger

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_path, alert: exception.message
+  end
+
   before_filter :authenticate_user!
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   # before_filter :authorize_developer
-
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_path, alert: exception.message
-  end
 
   unless Rails.application.config.consider_all_requests_local
     rescue_from Exception, with: lambda { |exception|
