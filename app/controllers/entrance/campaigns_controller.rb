c310cd8c73d59ce3198a5d048b1bf8e286889133
Vitaly Shlyaga <storkvist@storkvist.net>
2014-09-11 13:35:08 +0400

    ФИС

--- app/controllers/entrance/campaigns_controller.rb
+++ app/controllers/entrance/campaigns_controller.rb
@@ -95,8 +95,8 @@ class Entrance::CampaignsController < ApplicationController
 
   def report
     @applications = @campaign.applications.
-      find_all { |a| 8 == a.status_id && [11, 12].include?(a.form) && !a.payed? && %w(03 05).include?(a.direction.new_code.split('.')[1]) }
-      # find_all { |a| (a.number.include?('14-ГД') || a.number.include?('14-ЖД')) && 12922 != a.id }
+      find_all { |a| 8 == a.status_id && [11, 12].include?(a.form) && !a.payed? && %w(03 05).include?(a.direction.new_code.split('.')[1]) }.
+      find_all { |a| !a.number.include?('14-ОД') && a.benefits.empty? }
       # find_all { |a| %w(03 05).include?(a.direction.new_code.split('.')[1]) }.
       # find_all { |a| [11, 12].include?(a.form) }.
       # find_all { |a| !a.payed? }
@@ -129,6 +129,7 @@ class Entrance::CampaignsController < ApplicationController
                   xml.FinanceSourceID (application.competitive_group_item.payed? ? 15 : 14)
                   xml.EducationLevelID application.competitive_group_item.education_type_id
                   xml.IsBeneficiary application.benefits.any?
+                  xml.Stage ((Date.new(2014, 8, 5) == application.order.signing_date) ? 1 : 2)
                 end
               end
             end
