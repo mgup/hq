3533b21258e6d04b981b30bb8b9e2f6049088793
Vitaly Shlyaga <storkvist@storkvist.net>
2014-10-15 15:43:24 +0400

    ignore ActionController::InvalidAuthenticityToken

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -26,7 +26,8 @@ Rollbar.configure do |config|
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
-  %w(ActionController::RoutingError
+  %w(ActionController::InvalidAuthenticityToken
+     ActionController::RoutingError
      AbstractController::ActionNotFound
      ActiveRecord::RecordNotFound).each do |exception_name|
     config.exception_level_filters.merge!(exception_name => 'ignore')
