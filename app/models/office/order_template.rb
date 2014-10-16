9425d9f852b5e7aed9d934e5f449c7ee22aed6ea
Vitaly Shlyaga <storkvist@storkvist.net>
2014-10-16 14:02:42 +0400

    ~

--- app/models/office/order_template.rb
+++ app/models/office/order_template.rb
@@ -5,15 +5,13 @@ module Office
     alias_attribute :id, :template_id
     alias_attribute :name, :template_name
 
-    has_many :orders, class_name: Office::Order, foreign_key: :order_template
-    has_many :reasons, class_name: Office::Reason, foreign_key: :template_reason_template
-    has_many :causes, class_name: Office::Cause, foreign_key: :template_cause_template
+    has_many :orders, class_name: 'Office::Order', foreign_key: :order_template
+    has_many :reasons, class_name: 'Office::Reason', foreign_key: :template_reason_template
+    has_many :causes, class_name: 'Office::Cause', foreign_key: :template_cause_template
 
-    has_one :current_xsl, -> { order('order_xsl_time DESC').limit(1) }, class_name: Office::OrderXsl, foreign_key: :order_xsl_template
-
-    has_one :used_xsl, -> { order('order_xsl_time DESC').limit(1) }, class_name: Office::OrderXsl, foreign_key: :order_xsl_template
-
-    has_many :xsl, -> { order('order_xsl_time DESC').limit(1) }, class_name: Office::OrderXsl, foreign_key: :order_xsl_template
+    # TODO Откуда здесь различие? Это не логично. Нужно оставить что-то одно.
+    has_one :current_xsl, -> { order('order_xsl_time DESC').limit(1) }, class_name: 'Office::OrderXsl', foreign_key: :order_xsl_template
+    has_many :xsl, -> { order('order_xsl_time DESC').limit(1) }, class_name: 'Office::OrderXsl', foreign_key: :order_xsl_template
     accepts_nested_attributes_for :xsl, update_only: false
 
     default_scope { where(template_active: 1).order(:template_name) }
