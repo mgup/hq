91f3320a83a3ce31539273db1e639375bdbd0e14
Vitaly Shlyaga <storkvist@storkvist.net>
2014-10-23 15:53:30 +0400

    .

--- app/controllers/report/structure_controller.rb
+++ app/controllers/report/structure_controller.rb
@@ -16,7 +16,7 @@ class Report::StructureController < ApplicationController
     respond_to do |format|
       format.xlsx do
         response.headers['Content-Disposition'] =
-          %Q(attachment; filename="Контингент на #{Date.today}.xlsx")
+          %Q(attachment; filename="Контингент на #{Time.now}.xlsx")
       end
     end
   end
@@ -24,14 +24,15 @@ class Report::StructureController < ApplicationController
   private
 
   def get_all_actual_directions
-    Speciality.select('speciality_id, speciality_code, speciality_name, speciality_ntype').
-      joins(groups: :students).
+    Speciality.joins(groups: :students).
       where('student_group_status IN (?)',
             [Student::STATUS_STUDENT,
              Student::STATUS_COMPLETE,
              Student::STATUS_TRANSFERRED_DEBTOR,
              Student::STATUS_DEBTOR]).
-      group(:speciality_id)
+      group(:speciality_id).
+      order(:speciality_faculty, :speciality_name, :speciality_code).
+      where('speciality_id != 1') # FIXME Убрать после раскидывания аспирантуры.
   end
 
   def get_all_students
@@ -40,6 +41,8 @@ class Report::StructureController < ApplicationController
             [Student::STATUS_STUDENT,
              Student::STATUS_COMPLETE,
              Student::STATUS_TRANSFERRED_DEBTOR,
-             Student::STATUS_DEBTOR])
+             Student::STATUS_DEBTOR]).
+      with_group.where('group_course = 1'). # FIXME Убрать этот запрос.
+      where('group_speciality != 1')
   end
 end
