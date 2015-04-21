7abd8edd58ef08d1e5ad33e814c370a1973b3d4d
Anastasiya Sisaket <w-wonk@yandex.ru>
2015-04-21 16:59:44 +0300

    обновление review (9.0)

--- spec/factories/university_reviews.rb
+++ spec/factories/university_reviews.rb
@@ -2,5 +2,4 @@ FactoryGirl.define do
   factory :university_review do
     name "MyString"
   end
-
 end
--- spec/models/phonebook_spec.rb
+++ spec/models/phonebook_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Phonebook, :type => :model do
+RSpec.describe Phonebook, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
