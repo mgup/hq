6b481190f4d6c6dc1902e7f8c250513c13bf9c08
Vitaly Shlyaga <storkvist@storkvist.net>
2015-06-19 14:01:25 +0300

    .

--- app/models/entrance/achievement.rb
+++ app/models/entrance/achievement.rb
@@ -1,7 +1,7 @@
 class Entrance::Achievement < ActiveRecord::Base
   self.table_name_prefix = 'entrance_'
 
-  belongs_to :achievement_type, class_name: 'Entrance::AchievementType'
+  belongs_to :achievement_type, class_name: 'Entrance::AchievementType', foreign_key: :entrance_achievement_type_id
   belongs_to :entrant, class_name: 'Entrance::Entrant'
 
   def name
