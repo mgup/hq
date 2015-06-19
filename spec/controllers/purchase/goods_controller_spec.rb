66f610e764a108c2dbc92b12713e8327308055ee
Anastasiya Sisaket <w-wonk@yandex.ru>
2015-06-19 16:21:22 +0300

    заявки права доступа

--- spec/controllers/purchase/goods_controller_spec.rb
+++ spec/controllers/purchase/goods_controller_spec.rb
@@ -13,17 +13,99 @@ describe Purchase::GoodsController, type: :controller do
         get :index
       end
 
+      it 'должен быть успешный' do
+        expect(response).to be_success
+      end
+
+      it 'должна выводиться правильная страница' do
+        expect(response).to render_template(:index)
+      end
+
+      it 'в выводе должна присутствовать тестовый товар' do
+         expect(assigns(:goods)).to include(@purchase_good)
+      end
+    end
+
+    describe 'get new' do
+      before :each do
+        get :new
+      end
+
       it 'должен выполняться успешно' do
         expect(response).to be_success
       end
 
       it 'должен выводить правильное представление' do
-        expect(response).to render_template(:index)
+        expect(response).to render_template(:new)
       end
 
-      it 'в выводе должна присутствовать тестовая специальность' do
-        # good = assigns(:purchase_goods)
-        expect(assigns(:purchase_goods).to include(@purchase_good))
+      it 'должен содержать новую запись' do
+        expect(assigns(:good).new_record?).to be_truthy
+      end
+    end
+
+    describe 'GET "edit"' do
+      before :each do
+        @edited = FactoryGirl.create(:purchase_good)
+        get :edit, id: @edited.id
+      end
+
+      it 'должен выполняться успешно' do
+        expect(response).to be_success
+      end
+
+      it 'должен выводить правильное представление' do
+        expect(response).to render_template(:edit)
+      end
+
+      it 'должен находить правильный товар' do
+        expect(assigns(:good).to eq(@edited))
+      end
+    end
+
+    describe 'POST #create' do
+      context 'в случае ошибки' do
+        it 'должен перенаправить на создание' do
+          allow(Purchase::Good).to receive(:save).and_return(false)
+
+          post :create, purchase_good: {}
+          expect(response).to render_template :new
+        end
+      end
+    end
+
+    describe 'PUT #update' do
+      before :each do
+        @updated = FactoryGirl.create(:purchase_good)
+      end
+
+      context 'в случае успешного изменения' do
+        before :each do
+          allow(Purchase::Good).to receive(:update).and_return(true)
+
+          put :update, id: @updated, purchase_good: {}
+        end
+
+        it 'должен находить правильный товар' do
+          expect(assigns(:purchase_goods)).to eq(@updated)
+        end
+
+        it 'должен переходить на страницу с товарами' do
+          expect(flash[:notice]).not_to be_nil
+          expect(response).to redirect_to purchase_goods_path
+        end
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      before :each do
+        @deleted = FactoryGirl.create(:purchase_good)
+        expect { delete :destroy, id: @deleted }
+        .to change { Purchase::Good.count }.by(-1)
+      end
+
+      it 'должен находить правильный товар' do
+        expect(assigns(:good)).to eq(@deleted)
       end
     end
   end
--- spec/factories/purchase_goods.rb
+++ spec/factories/purchase_goods.rb
@@ -1,8 +1,7 @@
 require 'faker'
-require 'rails_helper'
 
 FactoryGirl.define do
-  factory :purchase_good, :class => 'Purchase::Good' do
+  factory :purchase_good, class: Purchase::Good do
     name { Faker::Lorem.sentence }
     demand { Faker::Lorem.sentence }
   end
