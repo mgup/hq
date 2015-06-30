9ae08922ce8d0c22b3b93c105ee296845a2ff4db
nutav <vau2007@rambler.ru>
2015-06-30 14:02:47 +0300

    +

--- config/sidebar_navigation.rb
+++ config/sidebar_navigation.rb
@@ -87,7 +87,7 @@ SimpleNavigation::Configuration.run do |navigation|
                  entrance_campaign_dates_path(Entrance::Campaign::CURRENT)
 
     primary.item :entrance_events, 'Вступительные испытания',
-                 entrance_campaign_event_path(Entrance::Campaign::CURRENT, id: 1)
+                 entrance_campaign_event_path(Entrance::Campaign::CURRENT, id: 115)
 
     # primary.item :entrants_results,
     #              '<span class="glyphicons inbox"></span> Результаты вступительных испытаний'.html_safe,
