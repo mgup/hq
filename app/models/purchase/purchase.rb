10846f0c32a7df9d98035bf66bf0a8f02f5fba0a
Anastasiya Sisaket <w-wonk@yandex.ru>
2015-07-07 12:37:49 +0300

    печатная форма закупки

--- app/models/purchase/purchase.rb
+++ app/models/purchase/purchase.rb
@@ -1,4 +1,7 @@
 class Purchase::Purchase < ActiveRecord::Base
+
+  RESPONSIBLE_POSITION_ROLES = [2, 22]
+
   has_many :purchase_line_items, class_name: 'Purchase::LineItem', foreign_key: :purchase_id, dependent: :destroy
   belongs_to :department, foreign_key: :dep_id
   validates :dep_id, presence: { message: 'Поле не может быть пустым!' }
