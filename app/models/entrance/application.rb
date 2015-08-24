a676096983e98f8bb621834d9c124abb0324764a
Vitaly Shlyaga <storkvist@storkvist.net>
2015-08-24 18:34:49 +0300

    ФИС

--- app/models/entrance/application.rb
+++ app/models/entrance/application.rb
@@ -809,7 +809,7 @@ class Entrance::Application < ActiveRecord::Base
               xml.IndividualAchievement do
                 xml.IAUID "individual_achievement_#{a.id}"
                 xml.IAName a.achievement_type.name
-                xml.IAMark a.score
+                xml.IAMark a.score if a.score.present?
                 xml.IADocumentUID "IA#{a.id}"
               end
             end
