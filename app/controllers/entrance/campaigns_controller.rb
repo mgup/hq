aea0284215821d3797fd96c1172eb7344ae1296b
Vitaly Shlyaga <storkvist@storkvist.net>
2016-04-19 15:27:33 +0300

    .

--- app/controllers/entrance/campaigns_controller.rb
+++ app/controllers/entrance/campaigns_controller.rb
@@ -340,7 +340,7 @@ class Entrance::CampaignsController < ApplicationController
                     elsif Date.new(2015, 7, 27) == application.order.signing_date
                       xml.Stage 1
                     else
-                      xml.Stage
+                      # xml.Stage 0
                     end
                   # end
                   # order_ids.push(application.order.id)
