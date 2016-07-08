9f0b23fb347799636d0d499e9279c0c5c6ff2365
Vitaly Shlyaga <storkvist@storkvist.net>
2016-07-08 11:07:38 +0300

    ФИС

--- app/controllers/entrance/campaigns_controller.rb
+++ app/controllers/entrance/campaigns_controller.rb
@@ -317,7 +317,7 @@ class Entrance::CampaignsController < ApplicationController
 
         apps = Entrance::Application.
           where(campaign_id: [12016, 22016, 32016, 42016]).
-          in_groups(4)
+          in_groups(5)
         @applications = apps[0]
 
         # @applications = Entrance::Application.where(
